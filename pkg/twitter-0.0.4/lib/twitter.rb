e6737ec8b07b9fd1ffd96a21074a100a6fb3cf7e
jnunemaker <jnunemaker@fe7eae16-9a24-0410-a59d-9e59979e88be>
2007-01-21 04:57:51 +0000

    added 0.0.4 release
    git-svn-id: http://svn.addictedtonew.com/public/gems/twitter@24 fe7eae16-9a24-0410-a59d-9e59979e88be

---
+++ pkg/twitter-0.0.4/lib/twitter.rb
@@ -0,0 +1,46 @@
+# = addicted to twitter
+# 
+# ... a sweet little diddy that helps you twitter your life away
+# 
+# 
+# == Install
+# 
+# $ sudo gem install twitter
+# 
+# == Examples
+#  
+# 	Twitter::Base.new('your email', 'your password').update('watching veronica mars')
+# 
+# 	# or you can use post
+# 	Twitter::Base.new('your email', 'your password').post('post works too')
+# 
+# 	puts "Public Timeline", "=" * 50
+# 	Twitter::Base.new('your email', 'your password').timeline(:public).each do |s|
+# 	  puts s.text, s.user.name
+# 	  puts
+# 	end
+# 
+# 	puts '', "Friends Timeline", "=" * 50
+# 	Twitter::Base.new('your email', 'your password').timeline.each do |s|
+# 	  puts s.text, s.user.name
+# 	  puts
+# 	end
+# 
+# 	puts '', "Friends", "=" * 50
+# 	Twitter::Base.new('your email', 'your password').friends.each do |u|
+# 	  puts u.name, u.status.text
+# 	  puts
+# 	end
+# 
+# 	puts '', "Followers", "=" * 50
+# 	Twitter::Base.new('your email', 'your password').followers.each do |u|
+# 	  puts u.name, u.status.text
+# 	  puts
+# 	end
+%w(uri net/http yaml rubygems hpricot).each { |f| require f }
+
+require 'twitter/version'
+require 'twitter/easy_class_maker'
+require 'twitter/base'
+require 'twitter/user'
+require 'twitter/status'
