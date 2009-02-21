6a8efc464dcb174e41b2eb0197a79e778dae1428
John Nunemaker <nunemaker@gmail.com>
2009-02-21 14:08:14 -0500

    verify_credentials now returns a Twitter::User.

--- lib/twitter/base.rb
+++ lib/twitter/base.rb
@@ -183,7 +183,7 @@ module Twitter
     # Verifies the credentials for the auth user.
     #   raises Twitter::CantConnect on failure.
     def verify_credentials
-      request('account/verify_credentials.xml', :auth => true)
+      users(request('account/verify_credentials.xml', :auth => true)).first
     end
     
     private      
