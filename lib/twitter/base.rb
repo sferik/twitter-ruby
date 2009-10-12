3770dfa67394886990bd3259bf6db8eb4fca9b76
Ryan McGeary <ryanongit@mcgeary.org>
2009-10-12 10:32:55 -0400

    Deprecated #replies in favor of #mentions
    * Twitter API dropped /statuses/replies for /statuses/mentions

--- lib/twitter/base.rb
+++ lib/twitter/base.rb
@@ -1,163 +1,171 @@
 module Twitter
   class Base
     extend Forwardable
-    
+
     def_delegators :client, :get, :post
-    
+
     attr_reader :client
-    
+
     def initialize(client)
       @client = client
     end
-    
+
     # Options: since_id, max_id, count, page, since
     def friends_timeline(query={})
       perform_get('/statuses/friends_timeline.json', :query => query)
     end
-    
+
     # Options: id, user_id, screen_name, since_id, max_id, page, since
     def user_timeline(query={})
       perform_get('/statuses/user_timeline.json', :query => query)
     end
-    
+
     def status(id)
       perform_get("/statuses/show/#{id}.json")
     end
-    
+
     # Options: in_reply_to_status_id
     def update(status, query={})
       perform_post("/statuses/update.json", :body => {:status => status}.merge(query))
     end
-    
+
+    # DEPRECATED: Use #mentions instead
+    #
     # Options: since_id, max_id, since, page
     def replies(query={})
+      warn("DEPRECATED: #replies is deprecated by Twitter; use #mentions instead")
       perform_get('/statuses/replies.json', :query => query)
     end
-    
+
+    # Options: since_id, max_id, count, page
+    def mentions(query={})
+      perform_get('/statuses/mentions.json', :query => query)
+    end
+
     def status_destroy(id)
       perform_post("/statuses/destroy/#{id}.json")
     end
-    
+
     # Options: id, user_id, screen_name, page
     def friends(query={})
       perform_get('/statuses/friends.json', :query => query)
     end
-    
+
     # Options: id, user_id, screen_name, page
     def followers(query={})
       perform_get('/statuses/followers.json', :query => query)
     end
-    
+
     def user(id, query={})
       perform_get("/users/show/#{id}.json", :query => query)
     end
-    
+
     # Options: since, since_id, page
     def direct_messages(query={})
       perform_get("/direct_messages.json", :query => query)
     end
-    
+
     # Options: since, since_id, page
     def direct_messages_sent(query={})
       perform_get("/direct_messages/sent.json", :query => query)
     end
-    
+
     def direct_message_create(user, text)
       perform_post("/direct_messages/new.json", :body => {:user => user, :text => text})
     end
-    
+
     def direct_message_destroy(id)
       perform_post("/direct_messages/destroy/#{id}.json")
     end
-    
+
     def friendship_create(id, follow=false)
       body = {}
       body.merge!(:follow => follow) if follow
       perform_post("/friendships/create/#{id}.json", :body => body)
     end
-    
+
     def friendship_destroy(id)
       perform_post("/friendships/destroy/#{id}.json")
     end
-    
+
     def friendship_exists?(a, b)
       perform_get("/friendships/exists.json", :query => {:user_a => a, :user_b => b})
     end
-    
+
     # Options: id, user_id, screen_name
     def friend_ids(query={})
       perform_get("/friends/ids.json", :query => query, :mash => false)
     end
-    
+
     # Options: id, user_id, screen_name
     def follower_ids(query={})
       perform_get("/followers/ids.json", :query => query, :mash => false)
     end
-    
+
     def verify_credentials
       perform_get("/account/verify_credentials.json")
     end
-    
+
     # Device must be sms, im or none
     def update_delivery_device(device)
       perform_post('/account/update_delivery_device.json', :body => {:device => device})
     end
-    
+
     # One or more of the following must be present:
-    #   profile_background_color, profile_text_color, profile_link_color, 
+    #   profile_background_color, profile_text_color, profile_link_color,
     #   profile_sidebar_fill_color, profile_sidebar_border_color
     def update_profile_colors(colors={})
       perform_post('/account/update_profile_colors.json', :body => colors)
     end
-    
+
     def rate_limit_status
       perform_get('/account/rate_limit_status.json')
     end
-    
+
     # One or more of the following must be present:
     #   name, email, url, location, description
     def update_profile(body={})
       perform_post('/account/update_profile.json', :body => body)
     end
-    
+
     # Options: id, page
     def favorites(query={})
       perform_get('/favorites.json', :query => query)
     end
-    
+
     def favorite_create(id)
       perform_post("/favorites/create/#{id}.json")
     end
-    
+
     def favorite_destroy(id)
       perform_post("/favorites/destroy/#{id}.json")
     end
-    
+
     def enable_notifications(id)
       perform_post("/notifications/follow/#{id}.json")
     end
-    
+
     def disable_notifications(id)
       perform_post("/notifications/leave/#{id}.json")
     end
-    
+
     def block(id)
       perform_post("/blocks/create/#{id}.json")
     end
-    
+
     def unblock(id)
       perform_post("/blocks/destroy/#{id}.json")
     end
-    
+
     def help
       perform_get('/help/test.json')
     end
-    
+
     private
       def perform_get(path, options={})
         Twitter::Request.get(self, path, options)
       end
-      
+
       def perform_post(path, options={})
         Twitter::Request.post(self, path, options)
       end
--- test/twitter/base_test.rb
+++ test/twitter/base_test.rb
@@ -8,24 +8,24 @@ class BaseTest < Test::Unit::TestCase
       oauth.stubs(:access_token).returns(@access_token)
       @twitter = Twitter::Base.new(oauth)
     end
-    
+
     context "initialize" do
       should "require a client" do
         @twitter.client.should respond_to(:get)
         @twitter.client.should respond_to(:post)
       end
     end
-    
+
     should "delegate get to the client" do
       @access_token.expects(:get).with('/foo').returns(nil)
       @twitter.get('/foo')
     end
-    
+
     should "delegate post to the client" do
       @access_token.expects(:post).with('/foo', {:bar => 'baz'}).returns(nil)
       @twitter.post('/foo', {:bar => 'baz'})
     end
-    
+
     context "hitting the api" do
       should "be able to get friends timeline" do
         stub_get('/statuses/friends_timeline.json', 'friends_timeline.json')
@@ -38,7 +38,7 @@ class BaseTest < Test::Unit::TestCase
         first.id.should == 1441588944
         first.favorited.should be(false)
       end
-      
+
       should "be able to get user timeline" do
         stub_get('/statuses/user_timeline.json', 'user_timeline.json')
         timeline = @twitter.user_timeline
@@ -47,44 +47,44 @@ class BaseTest < Test::Unit::TestCase
         first.text.should == 'Colder out today than expected. Headed to the Beanery for some morning wakeup drink. Latte or coffee...hmmm...'
         first.user.name.should == 'John Nunemaker'
       end
-      
+
       should "be able to get a status" do
         stub_get('/statuses/show/1441588944.json', 'status.json')
         status = @twitter.status(1441588944)
         status.user.name.should == 'John Nunemaker'
         status.id.should == 1441588944
       end
-      
+
       should "be able to update status" do
         stub_post('/statuses/update.json', 'status.json')
         status = @twitter.update('Rob Dyrdek is the funniest man alive. That is all.')
         status.user.name.should == 'John Nunemaker'
         status.text.should == 'Rob Dyrdek is the funniest man alive. That is all.'
       end
-      
-      should "be able to get replies" do
-        stub_get('/statuses/replies.json', 'replies.json')
-        replies = @twitter.replies
-        replies.size.should == 19
-        first = replies.first
+
+      should "be able to get mentions" do
+        stub_get('/statuses/mentions.json', 'mentions.json')
+        mentions = @twitter.mentions
+        mentions.size.should == 19
+        first = mentions.first
         first.user.name.should == '-oAk-'
         first.text.should == '@jnunemaker cold out today. cold yesterday. even colder today.'
       end
-      
+
       should "be able to get follower ids" do
         stub_get('/followers/ids.json', 'follower_ids.json')
         follower_ids = @twitter.follower_ids
         follower_ids.size.should == 1252
         follower_ids.first.should == 613
       end
-      
+
       should "be able to get friend ids" do
         stub_get('/friends/ids.json', 'friend_ids.json')
         friend_ids = @twitter.friend_ids
         friend_ids.size.should == 161
         friend_ids.first.should == 15323
       end
-      
+
       should "correctly hash statuses" do
         stub_get('/statuses/friends_timeline.json', 'friends_timeline.json')
         hashes = @twitter.friends_timeline.map{ |s| s.hash }
