365f8378b45c93ed6219ac49afec5c7f7eb85fe6
Daniel Neighman <has.sox@gmail.com>
2009-11-13 17:48:10 +1100

    Changes the Mash gem for the Hashie gem.  This is the Mash gem but renamed to not clash with extlib

--- lib/twitter/search.rb
+++ lib/twitter/search.rb
@@ -2,89 +2,89 @@ module Twitter
   class Search
     include HTTParty
     include Enumerable
-    
+
     attr_reader :result, :query
-    
+
     def initialize(q=nil, options={})
       @options = options
       clear
       containing(q) if q && q.strip != ''
     end
-    
+
     def user_agent
       @options[:user_agent] || 'Ruby Twitter Gem'
     end
-    
+
     def from(user)
       @query[:q] << "from:#{user}"
       self
     end
-    
+
     def to(user)
       @query[:q] << "to:#{user}"
       self
     end
-    
+
     def referencing(user)
       @query[:q] << "@#{user}"
       self
     end
     alias :references :referencing
     alias :ref :referencing
-    
+
     def containing(word)
       @query[:q] << "#{word}"
       self
     end
     alias :contains :containing
-    
+
     # adds filtering based on hash tag ie: #twitter
     def hashed(tag)
       @query[:q] << "##{tag}"
       self
     end
-    
+
     # lang must be ISO 639-1 code ie: en, fr, de, ja, etc.
     #
-    # when I tried en it limited my results a lot and took 
-    # out several tweets that were english so i'd avoid 
+    # when I tried en it limited my results a lot and took
+    # out several tweets that were english so i'd avoid
     # this unless you really want it
     def lang(lang)
       @query[:lang] = lang
       self
     end
-    
+
     # Limits the number of results per page
     def per_page(num)
       @query[:rpp] = num
       self
     end
-    
+
     # Which page of results to fetch
     def page(num)
       @query[:page] = num
       self
     end
-    
-    # Only searches tweets since a given id. 
+
+    # Only searches tweets since a given id.
     # Recommended to use this when possible.
     def since(since_id)
       @query[:since_id] = since_id
       self
     end
-    
+
     # Search tweets by latitude, longitude, and a given range.
     # Ranges like 25km and 50mi work.
     def geocode(lat, long, range)
       @query[:geocode] = [lat, long, range].join(',')
       self
     end
-    
+
     def max(id)
       @query[:max_id] = id
       self
     end
-    
+
     # Clears all the query filters to make a new search
     def clear
       @fetch = nil
@@ -92,18 +92,18 @@ module Twitter
       @query[:q] = []
       self
     end
-    
+
     def fetch(force=false)
       if @fetch.nil? || force
         query = @query.dup
         query[:q] = query[:q].join(' ')
         response = self.class.get('http://search.twitter.com/search.json', :query => query, :format => :json, :headers => {'User-Agent' => user_agent})
-        @fetch = Mash.new(response)
+        @fetch = Hashie::Mash.new(response)
       end
-      
+
       @fetch
     end
-    
+
     def each
       fetch()['results'].each { |r| yield r }
     end
--- lib/twitter/trends.rb
+++ lib/twitter/trends.rb
@@ -3,27 +3,27 @@ module Twitter
     include HTTParty
     base_uri 'search.twitter.com/trends'
     format :json
-    
+
     # :exclude => 'hashtags' to exclude hashtags
     def self.current(options={})
       mashup(get('/current.json', :query => options))
     end
-    
+
     # :exclude => 'hashtags' to exclude hashtags
     # :date => yyyy-mm-dd for specific date
     def self.daily(options={})
       mashup(get('/daily.json', :query => options))
     end
-    
+
     # :exclude => 'hashtags' to exclude hashtags
     # :date => yyyy-mm-dd for specific date
     def self.weekly(options={})
       mashup(get('/weekly.json', :query => options))
     end
-    
+
     private
       def self.mashup(response)
-        response['trends'].values.flatten.map { |t| Mash.new(t) }
+        response['trends'].values.flatten.map { |t| Hashie::Mash.new(t) }
       end
   end
 end
