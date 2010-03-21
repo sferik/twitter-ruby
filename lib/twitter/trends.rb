6f23c5eb3ffdac6eac65fa2b6d36f08aa7b6e1fb
Erik Michaels-Ober <sferik@gmail.com>
2010-03-21 08:39:53 -0700

    Added Twitter API versioning support for trends methods

--- lib/twitter/trends.rb
+++ lib/twitter/trends.rb
@@ -1,37 +1,37 @@
 module Twitter
   class Trends
     include HTTParty
-    base_uri 'search.twitter.com/trends'
+    base_uri "api.twitter.com/1/trends"
     format :json
 
     # :exclude => 'hashtags' to exclude hashtags
     def self.current(options={})
-      mashup(get('/current.json', :query => options))
+      mashup(get("/current.json", :query => options))
     end
 
     # :exclude => 'hashtags' to exclude hashtags
     # :date => yyyy-mm-dd for specific date
     def self.daily(options={})
-      mashup(get('/daily.json', :query => options))
+      mashup(get("/daily.json", :query => options))
     end
 
     # :exclude => 'hashtags' to exclude hashtags
     # :date => yyyy-mm-dd for specific date
     def self.weekly(options={})
-      mashup(get('/weekly.json', :query => options))
+      mashup(get("/weekly.json", :query => options))
     end
     
     def self.available(query={})
-      locations = get('http://api.twitter.com/1/trends/available.json', :query => query).map{|location| Hashie::Mash.new(location)}
+      locations = get("http://api.twitter.com/#{API_VERSION}/trends/available.json", :query => query).map{|location| Hashie::Mash.new(location)}
     end
     
     def self.for_location(woeid)
-      get("http://api.twitter.com/1/trends/#{woeid}.json").map{|location| Hashie::Mash.new(location)}
+      get("http://api.twitter.com/#{API_VERSION}/trends/#{woeid}.json").map{|location| Hashie::Mash.new(location)}
     end
 
     private
       def self.mashup(response)
-        response['trends'].values.flatten.map { |t| Hashie::Mash.new(t) }
+        response["trends"].values.flatten.map { |t| Hashie::Mash.new(t) }
       end
   end
 end
