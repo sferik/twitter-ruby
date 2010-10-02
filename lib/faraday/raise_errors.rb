794b6f828e470852577e13c8dd095a77587fb714
Erik Michaels-Ober <sferik@gmail.com>
2010-10-02 00:06:01 -0700

    Standardize format of error messages

--- lib/faraday/raise_errors.rb
+++ lib/faraday/raise_errors.rb
@@ -4,17 +4,17 @@ module Faraday
       env[:response].on_complete do |response|
         case response[:status].to_i
         when 400
-          raise Twitter::RateLimitExceeded, "(#{response[:status]}): #{response[:body]}"
+          raise Twitter::RateLimitExceeded, "#{response[:body]['error'] if response[:body]}"
         when 401
-          raise Twitter::Unauthorized, "(#{response[:status]}): #{response[:body]}"
+          raise Twitter::Unauthorized, "#{response[:body]['error'] if response[:body]}"
         when 403
-          raise Twitter::General, "(#{response[:status]}): #{response[:body]}"
+          raise Twitter::General, "#{response[:body]['error'] if response[:body]}"
         when 404
-          raise Twitter::NotFound, "(#{response[:status]}): #{response[:body]}"
+          raise Twitter::NotFound, "#{response[:body]['error'] if response[:body]}"
         when 500
-          raise Twitter::InformTwitter, "Twitter had an internal error. Please let them know in the group. (#{response[:status]}): #{response[:body]}"
+          raise Twitter::InformTwitter, "#{response[:body]['error'] if response[:body]}"
         when 502..503
-          raise Twitter::Unavailable, "(#{response[:status]}): #{response[:body]}"
+          raise Twitter::Unavailable, "#{response[:body]['error'] if response[:body]}"
         end
       end
     end
