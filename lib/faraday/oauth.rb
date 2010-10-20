e14f68fe4bdec5663130a2f8c31d8886f9250439
laserlemon <steve.richert@gmail.com>
2010-10-20 07:59:29 -0400

    Twitter doesn't support signed multipart form data.

--- lib/faraday/oauth.rb
+++ lib/faraday/oauth.rb
@@ -2,6 +2,7 @@ module Faraday
   class Request::OAuth < Faraday::Middleware
     def call(env)
       params = env[:body].is_a?(Hash) ? env[:body] : {}
+      params.clear if params.fetch(:headers, {})['Content-Type'].to_s.match(/multipart/)
       header = SimpleOAuth::Header.new(env[:method], env[:url], params, @options)
 
       env[:request_headers].merge!('Authorization' => header.to_s)
