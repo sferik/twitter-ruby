3e8d2ce0328748749180b5c0fea4658d92d5ff97
Erik Michaels-Ober <sferik@gmail.com>
2010-10-23 08:14:48 -0700

    Add specs for Twitter::Client::Block module

--- lib/twitter/client/block.rb
+++ lib/twitter/client/block.rb
@@ -4,13 +4,15 @@ module Twitter
       def block(user, options={})
         authenticate!
         merge_user_into_options!(user, options)
-        post('blocks/create', options)
+        response = post('blocks/create', options)
+        format.to_s.downcase == 'xml' ? response.user : response
       end
 
       def unblock(user, options={})
         authenticate!
         merge_user_into_options!(user, options)
-        delete('blocks/destroy', options)
+        response = delete('blocks/destroy', options)
+        format.to_s.downcase == 'xml' ? response.user : response
       end
 
       def block_exists?(user, options={})
@@ -26,12 +28,14 @@ module Twitter
 
       def blocking(options={})
         authenticate!
-        get('blocks/blocking', options)
+        response = get('blocks/blocking', options)
+        format.to_s.downcase == 'xml' ? response.users : response
       end
 
       def blocked_ids(options={})
         authenticate!
-        get('blocks/blocking/ids', options)
+        response = get('blocks/blocking/ids', options)
+        format.to_s.downcase == 'xml' ? response.ids.id.map{|id| id.to_i} : response
       end
     end
   end
