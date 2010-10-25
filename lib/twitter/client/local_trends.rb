8be42e8cdadf984b5cc477f2a163b6f2ace2401a
Erik Michaels-Ober <sferik@gmail.com>
2010-10-25 08:26:36 -0700

    Add specs for Twitter::Client::LocalTrends module

--- lib/twitter/client/local_trends.rb
+++ lib/twitter/client/local_trends.rb
@@ -2,11 +2,15 @@ module Twitter
   class Client
     module LocalTrends
       def trend_locations(options={})
-        get('trends/available', options)
+        authenticate
+        response = get('trends/available', options)
+        format.to_s.downcase == 'xml' ? response.locations : response
       end
 
       def local_trends(woeid=1, options={})
-        get("trends/#{woeid}", options)
+        authenticate
+        response = get("trends/#{woeid}", options)
+        format.to_s.downcase == 'xml' ? response.matching_trends.first.trend : response.first.trends.map{|trend| trend.name}
       end
     end
   end
