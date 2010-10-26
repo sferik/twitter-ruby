17c568a53a7d2af3c49bd9f7b70732ad76631eb6
Erik Michaels-Ober <sferik@gmail.com>
2010-10-26 16:24:23 -0700

    Add specs for Twitter::Client::Friendship module

--- lib/twitter/client/friendship.rb
+++ lib/twitter/client/friendship.rb
@@ -1,35 +1,43 @@
 module Twitter
   class Client
     module Friendship
-      def friendship_create(user, *args)
-        merge_user_into_options!(user, options)
+      def follow(user, *args)
         options = args.last.is_a?(Hash) ? args.pop : {}
+        merge_user_into_options!(user, options)
         follow = args.first || false
         response = post('friendships/create', options.merge(:follow => follow))
+        format.to_s.downcase == 'xml' ? response['user'] : response
       end
 
-      def friendship_destroy(user, options={})
+      alias :friendship_create :follow
+
+      def unfollow(user, options={})
         merge_user_into_options!(user, options)
         response = delete('friendships/destroy', options)
+        format.to_s.downcase == 'xml' ? response['user'] : response
       end
 
-      def friendship_exists?(user_a, user_b, options={})
-        response = get('friendships/exists', options.merge(:user_a => user_a, :user_b => user_b))
-        format == :xml ? !%w(0 false).include?(response['friends']) : response
+      alias :friendship_destroy :unfollow
+
+      def friendship_exists?(source, target, options={})
+        response = get('friendships/exists', options.merge(:user_a => source, :user_b => target))
+        format.to_s.downcase == 'xml' ? !%w(0 false).include?(response['friends']) : response
       end
 
       def friendship(options={})
-        response = get('friendships/show', options)
+        get('friendships/show', options)['relationship']
       end
 
       alias :friendship_show :friendship
 
       def friendships_incoming(options={})
         response = get('friendships/incoming', options)
+        format.to_s.downcase == 'xml' ? response['id_list']['ids']['id'].map{|id| id.to_i} : response['ids']
       end
 
       def friendships_outgoing(options={})
         response = get('friendships/outgoing', options)
+        format.to_s.downcase == 'xml' ? response['id_list']['ids']['id'].map{|id| id.to_i} : response['ids']
       end
     end
   end
---
+++ spec/twitter/client/friendship_spec.rb
@@ -0,0 +1,141 @@
+require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
+
+describe "Twitter::Client" do
+  Twitter::Configuration::VALID_FORMATS.each do |format|
+    context ".new(:format => '#{format}')" do
+      before do
+        @client = Twitter::Client.new(:format => format, :consumer_key => 'CK', :consumer_secret => 'CS', :oauth_token => 'OT', :oauth_token_secret => 'OS')
+      end
+
+      describe ".follow" do
+
+        before do
+          stub_post("friendships/create.#{format}").
+            to_return(:body => fixture("user.#{format}"), :headers => {:content_type => "application/#{format}; charset=utf-8"})
+        end
+
+        it "should get the correct resource" do
+          @client.follow("sferik")
+          a_post("friendships/create.#{format}").
+            should have_been_made
+        end
+
+        it "should return the befriended user" do
+          user = @client.follow("sferik")
+          user.name.should == "Erik Michaels-Ober"
+        end
+
+      end
+
+      describe ".unfollow" do
+
+        before do
+          stub_delete("friendships/destroy.#{format}").
+            with(:query => {"screen_name" => "sferik"}).
+            to_return(:body => fixture("user.#{format}"), :headers => {:content_type => "application/#{format}; charset=utf-8"})
+        end
+
+        it "should get the correct resource" do
+          @client.unfollow("sferik")
+          a_delete("friendships/destroy.#{format}").
+            with(:query => {"screen_name" => "sferik"}).
+            should have_been_made
+        end
+
+        it "should return the unfollowed" do
+          user = @client.friendship_destroy("sferik")
+          user.name.should == "Erik Michaels-Ober"
+        end
+
+      end
+
+      describe ".friendship_exists?" do
+
+        before do
+          stub_get("friendships/exists.#{format}?user_a=sferik&user_b=pengwynn").
+            to_return(:body => fixture("true.#{format}"), :headers => {:content_type => "application/#{format}; charset=utf-8"})
+          stub_get("friendships/exists.#{format}?user_a=pengwynn&user_b=sferik").
+            to_return(:body => fixture("false.#{format}"), :headers => {:content_type => "application/#{format}; charset=utf-8"})
+        end
+
+        it "should get the correct resource" do
+          @client.friendship_exists?("sferik", "pengwynn")
+          a_get("friendships/exists.#{format}?user_a=sferik&user_b=pengwynn").
+            should have_been_made
+        end
+
+        it "should return true if user_a follows user_b" do
+          friendship_exists = @client.friendship_exists?("sferik", "pengwynn")
+          friendship_exists.should be_true
+        end
+
+        it "should return false if user_a does not follows user_b" do
+          friendship_exists = @client.friendship_exists?("pengwynn", "sferik")
+          friendship_exists.should be_false
+        end
+
+      end
+
+      describe ".friendship" do
+
+        before do
+          stub_get("friendships/show.#{format}").
+            with(:query => {"source_screen_name" => "sferik", "target_screen_name" => "pengwynn"}).
+            to_return(:body => fixture("relationship.#{format}"), :headers => {:content_type => "application/#{format}; charset=utf-8"})
+        end
+
+        it "should get the correct resource" do
+          @client.friendship(:source_screen_name => "sferik", :target_screen_name => "pengwynn")
+          a_get("friendships/show.#{format}").
+            with(:query => {"source_screen_name" => "sferik", "target_screen_name" => "pengwynn"}).
+            should have_been_made
+        end
+
+        it "should return detailed information about the relationship between two users" do
+          relationship = @client.friendship(:source_screen_name => "sferik", :target_screen_name => "pengwynn")
+          relationship.source.screen_name.should == "sferik"
+        end
+
+      end
+
+      describe ".friendships_incoming" do
+
+        before do
+          stub_get("friendships/incoming.#{format}").
+            to_return(:body => fixture("id_list.#{format}"), :headers => {:content_type => "application/#{format}; charset=utf-8"})
+        end
+
+        it "should get the correct resource" do
+          @client.friendships_incoming
+          a_get("friendships/incoming.#{format}").
+            should have_been_made
+        end
+
+        it "should return an array of numeric IDs for every user who has a pending request to follow the authenticating user" do
+          id_list = @client.friendships_incoming
+          id_list.first.should == 146197851
+        end
+
+      end
+
+      describe ".friendships_outgoing" do
+
+        before do
+          stub_get("friendships/outgoing.#{format}").
+            to_return(:body => fixture("id_list.#{format}"), :headers => {:content_type => "application/#{format}; charset=utf-8"})
+        end
+
+        it "should get the correct resource" do
+          @client.friendships_outgoing
+          a_get("friendships/outgoing.#{format}").
+            should have_been_made
+        end
+
+        it "should return an array of numeric IDs for every protected user for whom the authenticating user has a pending follow request" do
+          id_list = @client.friendships_outgoing
+          id_list.first.should == 146197851
+        end
+      end
+    end
+  end
+end
