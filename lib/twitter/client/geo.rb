5e244ab51ab27654ee864bc4a1728603b10cbe26
Erik Michaels-Ober <sferik@gmail.com>
2010-10-26 17:15:20 -0700

    Add specs for Twitter::Client::Geo module

--- lib/twitter/client/geo.rb
+++ lib/twitter/client/geo.rb
@@ -1,23 +1,25 @@
 module Twitter
   class Client
     module Geo
-      def geo_search(options={})
-        get('geo/search', options)
+      def places_nearby(options={})
+        get('geo/search', options)['result']['places']
       end
 
-      def similar_places(options={})
-        get('geo/similar_places', options)
+      alias :geo_search :places_nearby
+
+      def places_similar(options={})
+        get('geo/similar_places', options)['result']['places']
       end
 
       def reverse_geocode(options={})
-        get('geo/reverse_geocode', options)
+        get('geo/reverse_geocode', options)['result']['places']
       end
 
       def place(id, options={})
         get("geo/id/#{id}", options)
       end
 
-      def create_place(options={})
+      def place_create(options={})
         post('geo/place', options)
       end
     end
