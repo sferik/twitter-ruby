f72b4099940dea995a02ee9dafe6dbe351ec3173
Erik Michaels-Ober <sferik@gmail.com>
2010-11-08 02:35:21 -0800

    Add default values for all configuration options

--- lib/twitter/configuration.rb
+++ lib/twitter/configuration.rb
@@ -4,24 +4,32 @@ require File.expand_path('../version', __FILE__)
 module Twitter
   module Configuration
     # Valid keys in the options hash when configuring a {Twitter::API}
-    VALID_OPTIONS_KEYS      = [:consumer_key, :consumer_secret, :oauth_token, :oauth_token_secret, :adapter, :endpoint, :format, :proxy, :search_endpoint, :user_agent].freeze
+    VALID_OPTIONS_KEYS         = [:consumer_key, :consumer_secret, :oauth_token, :oauth_token_secret, :adapter, :endpoint, :format, :proxy, :search_endpoint, :user_agent].freeze
     # Valid Twitter request/response formats
     #
     # @note Not all methods support all formats
-    VALID_FORMATS           = [:json, :xml].freeze
+    VALID_FORMATS              = [:json, :xml].freeze
 
+    # The consumer key set in {Twitter::Authentication}, unless overridden
+    DEFAULT_CONSUMER_KEY       = nil.freeze
+    # The consumer secret set in {Twitter::Authentication}, unless overridden
+    DEFAULT_CONSUMER_SECRET    = nil.freeze
+    # The OAuth token set in {Twitter::Authentication}, unless overridden
+    DEFAULT_OAUTH_TOKEN        = nil.freeze
+    # The OAuth token secret set in {Twitter::Authentication}, unless overridden
+    DEFAULT_OAUTH_TOKEN_SECRET = nil.freeze
     # The Faraday adapter set in {Twitter::Connection}, unless overridden
-    DEFAULT_ADAPTER         = Faraday.default_adapter.freeze
+    DEFAULT_ADAPTER            = Faraday.default_adapter.freeze
     # The Twitter REST API endpoint set in {Twitter::Connection}, unless overridden
-    DEFAULT_ENDPOINT        = 'https://api.twitter.com/1/'.freeze
+    DEFAULT_ENDPOINT           = 'https://api.twitter.com/1/'.freeze
     # The Twitter Search API endpoint set in {Twitter::Connection}, unless overridden
-    DEFAULT_SEARCH_ENDPOINT = 'https://search.twitter.com/'.freeze
+    DEFAULT_SEARCH_ENDPOINT    = 'https://search.twitter.com/'.freeze
     # The response format appended to the path in {Twitter::Request} and sent in the Accept header in {Twitter::Connection}, unless overridden
-    DEFAULT_FORMAT          = :json.freeze
+    DEFAULT_FORMAT             = :json.freeze
     # The proxy server set in {Twitter::Connection}, unless overridden
-    DEFAULT_PROXY           = nil.freeze
+    DEFAULT_PROXY              = nil.freeze
     # The user agent set in {Twitter::Connection}, unless overridden
-    DEFAULT_USER_AGENT      = "Twitter Ruby Gem #{Twitter::VERSION}".freeze
+    DEFAULT_USER_AGENT         = "Twitter Ruby Gem #{Twitter::VERSION}".freeze
 
     attr_accessor *VALID_OPTIONS_KEYS
 
