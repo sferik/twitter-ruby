0cbf3ff137a93b4efe42c776cc2deb4d569244b4
Erik Michaels-Ober <sferik@gmail.com>
2011-04-02 11:14:13 -0700

    Update response adapters for new faraday middleware API

--- lib/faraday/raise_http_4xx.rb
+++ lib/faraday/raise_http_4xx.rb
@@ -4,37 +4,30 @@ require 'faraday'
 module Faraday
   # @private
   class Response::RaiseHttp4xx < Response::Middleware
-    def self.register_on_complete(env)
-      env[:response].on_complete do |response|
-        case response[:status].to_i
-        when 400
-          raise Twitter::BadRequest.new(error_message(response), response[:response_headers])
-        when 401
-          raise Twitter::Unauthorized.new(error_message(response), response[:response_headers])
-        when 403
-          raise Twitter::Forbidden.new(error_message(response), response[:response_headers])
-        when 404
-          raise Twitter::NotFound.new(error_message(response), response[:response_headers])
-        when 406
-          raise Twitter::NotAcceptable.new(error_message(response), response[:response_headers])
-        when 420
-          raise Twitter::EnhanceYourCalm.new(error_message(response), response[:response_headers])
-        end
+    def on_complete(response)
+      case response[:status].to_i
+      when 400
+        raise Twitter::BadRequest.new(error_message(response), response[:response_headers])
+      when 401
+        raise Twitter::Unauthorized.new(error_message(response), response[:response_headers])
+      when 403
+        raise Twitter::Forbidden.new(error_message(response), response[:response_headers])
+      when 404
+        raise Twitter::NotFound.new(error_message(response), response[:response_headers])
+      when 406
+        raise Twitter::NotAcceptable.new(error_message(response), response[:response_headers])
+      when 420
+        raise Twitter::EnhanceYourCalm.new(error_message(response), response[:response_headers])
       end
-    end
-
-    def initialize(app)
-      super
-      @parser = nil
     end
 
     private
 
-    def self.error_message(response)
+    def error_message(response)
       "#{response[:method].to_s.upcase} #{response[:url].to_s}: #{response[:status]}#{error_body(response[:body])}"
     end
 
-    def self.error_body(body)
+    def error_body(body)
       if body.nil?
         nil
       elsif body['error']
