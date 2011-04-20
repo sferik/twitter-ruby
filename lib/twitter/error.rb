b74861e75f0cdf7eaafc37162e2f040ae27db002
Erik Michaels-Ober <sferik@gmail.com>
2011-04-19 19:09:05 -0700

    Fix copy/paste bug in Error#ratelimit_remaining

--- lib/twitter/error.rb
+++ lib/twitter/error.rb
@@ -9,15 +9,15 @@ module Twitter
     end
 
     def ratelimit_reset
-      Time.at(@http_headers.values_at('x-ratelimit-reset', 'X-RateLimit-Reset').detect {|value| value }.to_i)
+      Time.at(@http_headers.values_at('x-ratelimit-reset', 'X-RateLimit-Reset').detect{|value| value}.to_i)
     end
 
     def ratelimit_limit
-      @http_headers.values_at('x-ratelimit-limit', 'X-RateLimit-Limit').detect {|value| value }.to_i
+      @http_headers.values_at('x-ratelimit-limit', 'X-RateLimit-Limit').detect{|value| value}.to_i
     end
 
     def ratelimit_remaining
-      @http_headers.values_at('x-ratelimit-limit', 'X-RateLimit-Limit').detect {|value| value }.to_i
+      @http_headers.values_at('x-ratelimit-remaining', 'X-RateLimit-Remaining').detect{|value| value}.to_i
     end
 
     def retry_after
