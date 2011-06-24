0b91afaca9cb4bcee5398c5c9545ce88a86d82e7
Erik Michaels-Ober <sferik@gmail.com>
2011-06-23 19:03:23 -0700

    Wrap all module methods in class << self

--- lib/twitter.rb
+++ lib/twitter.rb
@@ -7,21 +7,22 @@ require 'twitter/base'
 
 module Twitter
   extend Configuration
+  class << self
+    # Alias for Twitter::Client.new
+    #
+    # @return [Twitter::Client]
+    def new(options={})
+      Twitter::Client.new(options)
+    end
 
-  # Alias for Twitter::Client.new
-  #
-  # @return [Twitter::Client]
-  def self.new(options={})
-    Twitter::Client.new(options)
-  end
+    # Delegate to Twitter::Client
+    def method_missing(method, *args, &block)
+      return super unless new.respond_to?(method)
+      new.send(method, *args, &block)
+    end
 
-  # Delegate to Twitter::Client
-  def self.method_missing(method, *args, &block)
-    return super unless new.respond_to?(method)
-    new.send(method, *args, &block)
-  end
-
-  def self.respond_to?(method, include_private = false)
-    new.respond_to?(method, include_private) || super(method, include_private)
+    def respond_to?(method, include_private = false)
+      new.respond_to?(method, include_private) || super(method, include_private)
+    end
   end
 end
