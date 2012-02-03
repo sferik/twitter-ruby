a8f3879cce7187666660911ce876e2746806a7e6
Oliver Azevedo Barnes <oli.azevedo.barnes@gmail.com>
2012-02-03 10:32:08 -0200

    checking for raw request on ParseJson#on_complete

--- lib/twitter/response/parse_json.rb
+++ lib/twitter/response/parse_json.rb
@@ -6,21 +6,23 @@ module Twitter
     class ParseJson < Faraday::Response::Middleware
 
       def parse(body)
-        unless (respond_to?(:env)) && env[:request][:raw]
-          body = case body
-                when ''
-                  nil
-                when 'true'
-                  true
-                when 'false'
-                  false
-                else
-                  ::MultiJson.decode(body) 
-                end 
-        end
-        body
+          case body
+          when ''
+            nil
+          when 'true'
+            true
+          when 'false'
+            false
+          else
+            ::MultiJson.decode(body) 
+          end 
       end
-
+      
+      def on_complete(env)
+        if respond_to? :parse
+          env[:body] = parse(env[:body]) unless env[:request][:raw] or [204,304].index env[:status]
+        end
+      end
     end
   end
 end
