3bc772fcc5782aac7df4071f8acb7ecc8b4cdba5
Erik Michaels-Ober <sferik@gmail.com>
2012-04-26 16:25:48 -0700

    Add tests for Twitter::List#created_at

--- spec/twitter/list_spec.rb
+++ spec/twitter/list_spec.rb
@@ -20,6 +20,17 @@ describe Twitter::List do
     end
   end
 
+  describe "#created_at" do
+    it "should return a Time when created_at is set" do
+      user = Twitter::List.new('created_at' => "Mon Jul 16 12:59:01 +0000 2007")
+      user.created_at.should be_a Time
+    end
+    it "should return nil when created_at is not set" do
+      user = Twitter::List.new
+      user.created_at.should be_nil
+    end
+  end
+
   describe "#user" do
     it "should return a User when user is set" do
       user = Twitter::List.new('user' => {}).user
