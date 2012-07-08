6f96c50477a046ba4e1c33d99a4b0ef7bb7ca650
Erik Michaels-Ober <sferik@gmail.com>
2012-07-07 17:02:57 -0700

    Bump version to 3.1.1

--- lib/twitter/version.rb
+++ lib/twitter/version.rb
@@ -2,7 +2,7 @@ module Twitter
   class Version
     MAJOR = 3 unless defined? MAJOR
     MINOR = 1 unless defined? MINOR
-    PATCH = 0 unless defined? PATCH
+    PATCH = 1 unless defined? PATCH
     PRE = nil unless defined? PRE
 
     class << self
