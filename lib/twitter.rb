23cfaf9dec4bc58fd9b3fd8366fb0e087c7f1e51
Erik Michaels-Ober <sferik@gmail.com>
2012-08-12 20:16:28 -0400

    Always define respond_to_missing? when overriding method_missing
    See: http://robots.thoughtbot.com/post/28335346416/always-define-respond-to-missing-when-overriding

--- lib/twitter.rb
+++ lib/twitter.rb
@@ -16,15 +16,14 @@ module Twitter
       end
     end
 
-    def respond_to?(method, include_private=false)
-      self.client.respond_to?(method, include_private) || super
-    end
+    def respond_to_missing?(method_name, include_private=false); client.respond_to?(method_name, include_private); end if RUBY_VERSION >= "1.9"
+    def respond_to?(method_name, include_private=false); client.respond_to?(method_name, include_private) || super; end if RUBY_VERSION < "1.9"
 
   private
 
-    def method_missing(method, *args, &block)
-      return super unless self.client.respond_to?(method)
-      self.client.send(method, *args, &block)
+    def method_missing(method_name, *args, &block)
+      return super unless client.respond_to?(method_name)
+      client.send(method_name, *args, &block)
     end
 
   end
