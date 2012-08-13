1f9c9c448e4882765560f2899dec8856433ef889
Erik Michaels-Ober <sferik@gmail.com>
2012-08-13 13:32:32 -0400

    Specify keys statically to fix YARD documentation

--- lib/twitter/configurable.rb
+++ lib/twitter/configurable.rb
@@ -2,31 +2,24 @@ require 'twitter/default'
 
 module Twitter
   module Configurable
-
-    AUTH_KEYS = [
-      :consumer_key,
-      :consumer_secret,
-      :oauth_token,
-      :oauth_token_secret,
-    ] unless defined? AUTH_KEYS
-
-    attr_writer *AUTH_KEYS
-
-    CONFIG_KEYS = [
-      :endpoint,
-      :media_endpoint,
-      :search_endpoint,
-      :connection_options,
-      :identity_map,
-      :middleware,
-    ] unless defined? CONFIG_KEYS
-
-    attr_accessor *CONFIG_KEYS
+    attr_writer :consumer_key, :consumer_secret, :oauth_token, :oauth_token_secret
+    attr_accessor :endpoint, :media_endpoint, :search_endpoint, :connection_options, :identity_map, :middleware
 
     class << self
 
       def keys
-        @keys ||= AUTH_KEYS + CONFIG_KEYS
+        @keys ||= [
+          :consumer_key,
+          :consumer_secret,
+          :oauth_token,
+          :oauth_token_secret,
+          :endpoint,
+          :media_endpoint,
+          :search_endpoint,
+          :connection_options,
+          :identity_map,
+          :middleware,
+        ]
       end
 
     end
@@ -58,9 +51,6 @@ module Twitter
   private
 
     # @return [Hash]
-    # @todo In version 4, rename to oauth_token to token and oauth_token_secret
-    #   to token_secret and rewrite as:
-    #     options.select{|key| AUTH_KEYS.include?(key)}
     def credentials
       {
         :consumer_key => @consumer_key,
