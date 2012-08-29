30ee1c733cfea091f60b18a51d01eab1d0cc6f30
Christopher Burnett <cburnett@twitter.com>
2012-08-29 11:36:30 -0700

    Do not attempt to parse redirects

--- lib/twitter/response/parse_json.rb
+++ lib/twitter/response/parse_json.rb
@@ -20,7 +20,7 @@ module Twitter
 
       def on_complete(env)
         if respond_to?(:parse)
-          env[:body] = parse(env[:body]) unless [204, 304].include?(env[:status])
+          env[:body] = parse(env[:body]) unless [204, 301, 302, 304].include?(env[:status])
         end
       end
 
