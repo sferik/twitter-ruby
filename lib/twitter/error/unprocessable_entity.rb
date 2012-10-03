fca4d174e8237655c82992edf67fcc846497fd54
Erik Michaels-Ober <sferik@gmail.com>
2012-10-03 01:17:27 -0700

    Add Twitter::Error::UnprocessableEntity

---
+++ lib/twitter/error/unprocessable_entity.rb
@@ -0,0 +1,10 @@
+require 'twitter/error/client_error'
+
+module Twitter
+  class Error
+    # Raised when Twitter returns the HTTP status code 404
+    class UnprocessableEntity < Twitter::Error::ClientError
+      HTTP_STATUS_CODE = 422
+    end
+  end
+end
--- lib/twitter/response/raise_error.rb
+++ lib/twitter/response/raise_error.rb
@@ -8,6 +8,7 @@ require 'twitter/error/not_acceptable'
 require 'twitter/error/not_found'
 require 'twitter/error/service_unavailable'
 require 'twitter/error/unauthorized'
+require 'twitter/error/unprocessable_entity'
 
 module Twitter
   module Response
