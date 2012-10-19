f480121ec15c8022d0c96eac12f65c26560a9c96
Alexander <aheaven87@gmail.com>
2012-10-19 17:45:32 +0300

    [^] Add abort_on_exception to threaded_map

--- lib/twitter/core_ext/enumerable.rb
+++ lib/twitter/core_ext/enumerable.rb
@@ -2,9 +2,11 @@ module Enumerable
 
   def threaded_map
     threads = []
+    Thread.abort_on_exception = true
     each do |object|
       threads << Thread.new{yield object}
     end
+    Thread.abort_on_exception = false
     threads.map(&:value)
   end
 
