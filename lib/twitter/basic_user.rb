c0b14e38c7497a82d2108fba3e81b546c9caf40b
Erik Michaels-Ober <sferik@gmail.com>
2012-10-20 08:19:07 -0700

    Add handle, username, and user_name aliases to Twitter::BasicUser#screen_name

--- lib/twitter/basic_user.rb
+++ lib/twitter/basic_user.rb
@@ -4,5 +4,8 @@ module Twitter
   class BasicUser < Twitter::Identity
     attr_reader :following, :screen_name
     alias following? following
+    alias handle screen_name
+    alias username screen_name
+    alias user_name screen_name
   end
 end
