6ef27768d3d7284b9d8856adfc59dfab2c7227eb
Erik Michaels-Ober <sferik@gmail.com>
2012-12-21 08:52:45 -0800

    Remove class_eval wrapper method

--- lib/twitter/core_ext/kernel.rb
+++ lib/twitter/core_ext/kernel.rb
@@ -3,9 +3,4 @@ module Kernel
   # Returns the object's singleton class (exists in Ruby 1.9.2)
   def singleton_class; class << self; self; end; end unless method_defined?(:singleton_class)
 
-  # class_eval on an object acts like singleton_class.class_eval.
-  def class_eval(*args, &block)
-    singleton_class.class_eval(*args, &block)
-  end
-
 end
