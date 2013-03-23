7538dba98dbc45fbea8e220c4806cb11e51145ae
Keith <keith.strand@gmail.com>
2013-03-22 19:05:40 -0700

    create spec file for oauth interface

--- lib/twitter/api/oauth.rb
+++ lib/twitter/api/oauth.rb
@@ -1,11 +1,25 @@
+require 'twitter/api/utils'
+
 module Twitter
   module API
     module OAuth
-      
-      def token
+    	include Twitter::API::Utils
+
+
+    	# Allows a registered application to obtain an OAuth 2 Bearer Token, 
+    	# which can be used to make API requests on an application's own behalf, 
+    	# without a user context. This is called Application-only authentication.
+    	# @raise [Twitter::Error::Unauthorized] Error raised when supplied user credentials are not valid.
+    	#
+    	# 
+      #
+
+
+      def bearer_token
+      	
       end
 
-      def invalidate
+      def invalidate_token
       end
   
     end
---
+++ spec/twitter/api/oauth_spec.rb
@@ -0,0 +1,14 @@
+require 'helper'
+
+describe Twitter::API::OAuth do
+
+  before do
+    @client = Twitter::Client.new
+  end
+ 
+  describe "#token" do
+  	stub_post("/1.1//oauth2/token/16129012.json").to_return(:body => fixture("token.json"), :headers => {:content_type => "application/json; charset=utf-8"})
+  end
+
+
+end
