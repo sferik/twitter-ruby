06a30926f830d5746572fa91aff7afe7da6f940f
Keith <keith.strand@gmail.com>
2013-03-22 19:50:31 -0700

    require oauth in client.rb

--- lib/twitter/api/oauth.rb
+++ lib/twitter/api/oauth.rb
@@ -5,18 +5,17 @@ module Twitter
     module OAuth
     	include Twitter::API::Utils
 
-
     	# Allows a registered application to obtain an OAuth 2 Bearer Token, 
     	# which can be used to make API requests on an application's own behalf, 
     	# without a user context. This is called Application-only authentication.
-    	# @raise [Twitter::Error::Unauthorized] Error raised when supplied user credentials are not valid.
+    	# 
     	#
     	# 
       #
 
 
       def bearer_token
-      	
+
       end
 
       def invalidate_token
--- spec/twitter/api/oauth_spec.rb
+++ spec/twitter/api/oauth_spec.rb
@@ -6,8 +6,8 @@ describe Twitter::API::OAuth do
     @client = Twitter::Client.new
   end
  
-  describe "#token" do
-  	stub_post("/1.1//oauth2/token/16129012.json").to_return(:body => fixture("token.json"), :headers => {:content_type => "application/json; charset=utf-8"})
+  describe "#bearer_token" do
+  	#stub_post("/1.1//oauth2/token/16129012.json").to_return(:body => fixture("token.json"), :headers => {:content_type => "application/json; charset=utf-8"})
   end
 
 
