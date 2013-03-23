cb0b8e8a86fa87de23fae0bdce9f359eadb5db22
Keith <keith.strand@gmail.com>
2013-03-22 20:03:02 -0700

    adjusted stub_post to reflect questions about json objects needed

--- spec/twitter/api/oauth_spec.rb
+++ spec/twitter/api/oauth_spec.rb
@@ -7,7 +7,7 @@ describe Twitter::API::OAuth do
   end
  
   describe "#bearer_token" do
-  	#stub_post("/1.1//oauth2/token/16129012.json").to_return(:body => fixture("token.json"), :headers => {:content_type => "application/json; charset=utf-8"})
+  	#stub_post("/1.1/oauth2/token/??????.json").to_return(:body => fixture("???.json"), :headers => {:content_type => "application/json; charset=utf-8"})
   end
 
 
