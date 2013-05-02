fd2f5d4b49821c5853490db591797acc7b0bcbf1
Paul Walker <github@paulwalker.tv>
2013-05-02 13:30:12 -0700

    clean up comments

--- lib/twitter/api/application.rb
+++ lib/twitter/api/application.rb
@@ -5,15 +5,13 @@ module Twitter
     module Application
       include Twitter::API::Utils
 
-      # Returns {https://twitter.com/application/rate_limit_status Twitter's Terms of Service}
+      # Returns Rate Limit Data for the current application
       #
       # @see https://dev.twitter.com/docs/api/1.1/get/application/rate_limit_status
       # @rate_limited Yes
       # @authentication User or App Context
       # @raise [Twitter::Error::Unauthorized] Error raised when credentials are not valid.
       # @return [String]
-      # @example Return {https://twitter.com/tos Twitter's Terms of Service}
-      #   Twitter.tos
       def rate_limit_status(options={})
         get("/1.1/application/rate_limit_status.json", options)[:body]
       end
