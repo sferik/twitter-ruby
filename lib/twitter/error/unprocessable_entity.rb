0b55611888568d48fa5c18bbe629f4ef77da6581
Steve Agalloco <steve.agalloco@gmail.com>
2013-06-11 15:36:50 -0400

    fix response code in unprocessable_entity comment, fixes #409

--- lib/twitter/error/unprocessable_entity.rb
+++ lib/twitter/error/unprocessable_entity.rb
@@ -2,7 +2,7 @@ require 'twitter/error/client_error'
 
 module Twitter
   class Error
-    # Raised when Twitter returns the HTTP status code 404
+    # Raised when Twitter returns the HTTP status code 422
     class UnprocessableEntity < Twitter::Error::ClientError
       HTTP_STATUS_CODE = 422
     end
