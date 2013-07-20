c90fd49f1a2dc1b39142a1e05734adaeac02fcb3
Erik Michaels-Ober <sferik@gmail.com>
2013-07-20 14:48:24 +0200

    Fix typos in documentation

--- lib/twitter/api/direct_messages.rb
+++ lib/twitter/api/direct_messages.rb
@@ -54,7 +54,7 @@ module Twitter
       # @authentication Requires user context
       # @raise [Twitter::Error::Unauthorized] Error raised when supplied user credentials are not valid.
       # @return [Twitter::DirectMessage] The requested messages.
-      # @param id [Integer] A Tweet IDs.
+      # @param id [Integer] A direct message ID.
       # @param options [Hash] A customizable set of options.
       # @example Return the direct message with the id 1825786345
       #   Twitter.direct_message(1825786345)
@@ -83,14 +83,14 @@ module Twitter
       #   Returns direct messages
       #
       #   @see https://dev.twitter.com/docs/api/1.1/get/direct_messages/show
-      #   @param ids [Array<Integer>, Set<Integer>] An array of Tweet IDs.
+      #   @param ids [Array<Integer>, Set<Integer>] An array of direct message IDs.
       #   @example Return the direct message with the id 1825786345
       #     Twitter.direct_messages(1825786345)
       # @overload direct_messages(*ids, options)
       #   Returns direct messages
       #
       #   @see https://dev.twitter.com/docs/api/1.1/get/direct_messages/show
-      #   @param ids [Array<Integer>, Set<Integer>] An array of Tweet IDs.
+      #   @param ids [Array<Integer>, Set<Integer>] An array of direct message IDs.
       #   @param options [Hash] A customizable set of options.
       def direct_messages(*args)
         arguments = Twitter::API::Arguments.new(args)
@@ -112,11 +112,11 @@ module Twitter
       # @raise [Twitter::Error::Unauthorized] Error raised when supplied user credentials are not valid.
       # @return [Array<Twitter::DirectMessage>] Deleted direct message.
       # @overload direct_message_destroy(*ids)
-      #   @param ids [Array<Integer>, Set<Integer>] An array of Tweet IDs.
+      #   @param ids [Array<Integer>, Set<Integer>] An array of direct message IDs.
       #   @example Destroys the direct message with the ID 1825785544
       #     Twitter.direct_message_destroy(1825785544)
       # @overload direct_message_destroy(*ids, options)
-      #   @param ids [Array<Integer>, Set<Integer>] An array of Tweet IDs.
+      #   @param ids [Array<Integer>, Set<Integer>] An array of direct message IDs.
       #   @param options [Hash] A customizable set of options.
       def direct_message_destroy(*args)
         threaded_object_from_response(Twitter::DirectMessage, :post, "/1.1/direct_messages/destroy.json", args)
--- lib/twitter/api/saved_searches.rb
+++ lib/twitter/api/saved_searches.rb
@@ -22,14 +22,14 @@ module Twitter
       #   Retrieve the data for saved searches owned by the authenticating user
       #
       #   @see https://dev.twitter.com/docs/api/1.1/get/saved_searches/show/:id
-      #   @param ids [Array<Integer>, Set<Integer>] An array of Tweet IDs.
+      #   @param ids [Array<Integer>, Set<Integer>] An array of saved search IDs.
       #   @example Retrieve the data for a saved search owned by the authenticating user with the ID 16129012
       #     Twitter.saved_search(16129012)
       # @overload saved_search(*ids, options)
       #   Retrieve the data for saved searches owned by the authenticating user
       #
       #   @see https://dev.twitter.com/docs/api/1.1/get/saved_searches/show/:id
-      #   @param ids [Array<Integer>, Set<Integer>] An array of Tweet IDs.
+      #   @param ids [Array<Integer>, Set<Integer>] An array of saved search IDs.
       #   @param options [Hash] A customizable set of options.
       def saved_searches(*args)
         arguments = Twitter::API::Arguments.new(args)
@@ -49,7 +49,7 @@ module Twitter
       # @authentication Requires user context
       # @raise [Twitter::Error::Unauthorized] Error raised when supplied user credentials are not valid.
       # @return [Twitter::SavedSearch] The saved searches.
-      # @param id [Integer] A Tweet IDs.
+      # @param id [Integer] The ID of the saved search.
       # @param options [Hash] A customizable set of options.
       # @example Retrieve the data for a saved search owned by the authenticating user with the ID 16129012
       #   Twitter.saved_search(16129012)
@@ -81,11 +81,11 @@ module Twitter
       # @raise [Twitter::Error::Unauthorized] Error raised when supplied user credentials are not valid.
       # @return [Array<Twitter::SavedSearch>] The deleted saved searches.
       # @overload saved_search_destroy(*ids)
-      #   @param ids [Array<Integer>, Set<Integer>] An array of Tweet IDs.
+      #   @param ids [Array<Integer>, Set<Integer>] An array of saved search IDs.
       #   @example Destroys a saved search for the authenticated user with the ID 16129012
       #     Twitter.saved_search_destroy(16129012)
       # @overload saved_search_destroy(*ids, options)
-      #   @param ids [Array<Integer>, Set<Integer>] An array of Tweet IDs.
+      #   @param ids [Array<Integer>, Set<Integer>] An array of saved search IDs.
       #   @param options [Hash] A customizable set of options.
       def saved_search_destroy(*args)
         arguments = Twitter::API::Arguments.new(args)
