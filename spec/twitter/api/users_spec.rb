855e8a9d61549506cd631a1d80d60bd08d8b482f
Erik Michaels-Ober <sferik@gmail.com>
2013-07-25 12:36:33 +0200

    Remove Twitter::Cursor#all

--- spec/twitter/api/users_spec.rb
+++ spec/twitter/api/users_spec.rb
@@ -186,12 +186,12 @@ describe Twitter::API::Users do
       expect(blocking.users.first).to be_a Twitter::User
       expect(blocking.users.first.id).to eq 7505382
     end
-    context "with all" do
+    context "with each" do
       before do
         stub_get("/1.1/blocks/list.json").with(:query => {:cursor => "1322801608223717003"}).to_return(:body => fixture("users_list2.json"), :headers => {:content_type => "application/json; charset=utf-8"})
       end
       it "requests the correct resource" do
-        @client.blocking.all
+        @client.blocking.each{}
         expect(a_get("/1.1/blocks/list.json").with(:query => {:cursor => "-1"})).to have_been_made
         expect(a_get("/1.1/blocks/list.json").with(:query => {:cursor => "1322801608223717003"})).to have_been_made
       end
@@ -212,12 +212,12 @@ describe Twitter::API::Users do
       expect(blocked_ids.ids).to be_an Array
       expect(blocked_ids.ids.first).to eq 14100886
     end
-    context "with all" do
+    context "with each" do
       before do
         stub_get("/1.1/blocks/ids.json").with(:query => {:cursor => "1305102810874389703"}).to_return(:body => fixture("ids_list2.json"), :headers => {:content_type => "application/json; charset=utf-8"})
       end
       it "requests the correct resource" do
-        @client.blocked_ids.all
+        @client.blocked_ids.each{}
         expect(a_get("/1.1/blocks/ids.json").with(:query => {:cursor => "-1"})).to have_been_made
         expect(a_get("/1.1/blocks/ids.json").with(:query => {:cursor => "1305102810874389703"})).to have_been_made
       end
