499a972805b9dbe1376890de9dd61b4422489e68
Yannick Schutz <yannick.schutz@gmail.com>
2013-07-29 16:47:03 +0200

    Handle error codes in Twitter::Error Twitter::ServerError and Twitter::ClientError can parse the response and find error codes.

--- spec/twitter/error/client_error_spec.rb
+++ spec/twitter/error/client_error_spec.rb
@@ -18,6 +18,33 @@ describe Twitter::Error::ClientError do
         end
       end
     end
+    context "when HTTP status is #{status} and body is errors" do
+      context "when errors is an array of hashes" do
+        context "when error code is nil" do
+          before do
+            body_message = '{"errors":[{"message":"Client Error"}]}'
+            stub_get("/1.1/statuses/user_timeline.json").with(:query => {:screen_name => "sferik"}).to_return(:status => status, :body => body_message)
+          end
+          it "raises #{exception.name}" do
+            expect{@client.user_timeline("sferik")}.to raise_error { |error|
+              expect(error.code).to eq nil
+            }
+          end
+
+          context "when error code is 187" do
+            before do
+              body_message = '{"errors":[{"message":"Client Error","code": 187 }]}'
+              stub_get("/1.1/statuses/user_timeline.json").with(:query => {:screen_name => "sferik"}).to_return(:status => status, :body => body_message)
+            end
+            it "raises #{exception.name}" do
+              expect{@client.user_timeline("sferik")}.to raise_error { |error|
+                expect(error.code).to eq 187
+              }
+            end
+          end
+        end
+      end
+    end
   end
 
 end
