801346f09a02e409abf3af16b9421161f0d92ab6
Erik Michaels-Ober <sferik@gmail.com>
2013-08-02 08:58:00 +0200

    Define Twitter::NullObject#respond_to_missing?

--- lib/twitter/null_object.rb
+++ lib/twitter/null_object.rb
@@ -4,14 +4,17 @@ module Twitter
   class NullObject
     include Singleton
 
+    def nil?
+      true
+    end
+
     def method_missing(*args, &block)
       nil
     end
 
-    def null?
+    def respond_to_missing?(method_name, include_private=false)
       true
     end
-    alias nil? null?
 
   end
 end
--- spec/twitter/null_object_spec.rb
+++ spec/twitter/null_object_spec.rb
@@ -2,10 +2,17 @@ require 'helper'
 
 describe Twitter::NullObject do
 
-  describe "#null?" do
+  describe "#nil?" do
     it "returns true" do
       null_object = Twitter::NullObject.instance
       expect(null_object.null?).to be_true
+    end
+  end
+
+  describe "#respond_to?" do
+    it "returns true" do
+      null_object = Twitter::NullObject.instance
+      expect(null_object.respond_to?(:method)).to be_true
     end
   end
 
