sixdegrees/twitter

Implement parallel map using Celluloid::Futures

lib/twitter/api/favorites.rb
@@ -56,7 +56,7 @@ module Twitter
       #   @param ids [Array<Integer>, Set<Integer>] An array of Tweet IDs.
       #   @param options [Hash] A customizable set of options.
       def unfavorite(*args)
-        threaded_object_from_response(Twitter::Tweet, :post, "/1.1/favorites/destroy.json", args)
+        parallel_object_from_response(Twitter::Tweet, :post, "/1.1/favorites/destroy.json", args)
       end
       alias favorite_destroy unfavorite
       alias favourite_destroy unfavorite
@@ -78,7 +78,7 @@ module Twitter
       #   @param options [Hash] A customizable set of options.
       def favorite(*args)
         arguments = Twitter::API::Arguments.new(args)
-        arguments.flatten.threaded_map do |id|
+        arguments.flatten.pmap do |id|
           begin
             object_from_response(Twitter::Tweet, :post, "/1.1/favorites/create.json", arguments.options.merge(id: id))
           rescue Twitter::Error::Forbidden => error
@@ -108,7 +108,7 @@ module Twitter
       #   @param options [Hash] A customizable set of options.
       def favorite!(*args)
         arguments = Twitter::API::Arguments.new(args)
-        arguments.flatten.threaded_map do |id|
+        arguments.flatten.pmap do |id|
           begin
             object_from_response(Twitter::Tweet, :post, "/1.1/favorites/create.json", arguments.options.merge(id: id))
           rescue Twitter::Error::Forbidden => error

lib/twitter/api/utils.rb
@@ -14,9 +14,9 @@ module Twitter
       # @param path [String]
       # @param args [Array]
       # @return [Array<Twitter::User>]
-      def threaded_user_objects_from_response(request_method, path, args)
+      def parallel_user_objects_from_response(request_method, path, args)
         arguments = Twitter::API::Arguments.new(args)
-        arguments.flatten.threaded_map do |user|
+        arguments.flatten.pmap do |user|
           object_from_response(Twitter::User, request_method, path, merge_user(arguments.options, user))
         end
       end
@@ -66,9 +66,9 @@ module Twitter
       # @param path [String]
       # @param args [Array]
       # @return [Array]
-      def threaded_object_from_response(klass, request_method, path, args)
+      def parallel_object_from_response(klass, request_method, path, args)
         arguments = Twitter::API::Arguments.new(args)
-        arguments.flatten.threaded_map do |id|
+        arguments.flatten.pmap do |id|
           object_from_response(klass, request_method, path, arguments.options.merge(id: id))
         end
       end

sferik/twitter-ruby

lib/twitter/api/favorites.rb
@@ -56,7 +56,7 @@ module Twitter
       #   @param tweets [Enumerable<Integer, String, URI, Twitter::Tweet>] A collection of Tweet IDs, URIs, or objects.
       #   @param options [Hash] A customizable set of options.
       def unfavorite(*args)
-        threaded_object_from_response(Twitter::Tweet, :post, "/1.1/favorites/destroy.json", args)
+        parallel_object_from_response(Twitter::Tweet, :post, "/1.1/favorites/destroy.json", args)
       end
       alias favorite_destroy unfavorite
       alias favourite_destroy unfavorite
@@ -78,7 +78,7 @@ module Twitter
       #   @param options [Hash] A customizable set of options.
       def favorite(*args)
         arguments = Twitter::API::Arguments.new(args)
-        arguments.flatten.threaded_map do |tweet|
+        arguments.flatten.pmap do |tweet|
           id = extract_id(tweet)
           begin
             object_from_response(Twitter::Tweet, :post, "/1.1/favorites/create.json", arguments.options.merge(:id => id))
@@ -109,7 +109,7 @@ module Twitter
       #   @param options [Hash] A customizable set of options.
       def favorite!(*args)
         arguments = Twitter::API::Arguments.new(args)
-        arguments.flatten.threaded_map do |tweet|
+        arguments.flatten.pmap do |tweet|
           id = extract_id(tweet)
           begin
             object_from_response(Twitter::Tweet, :post, "/1.1/favorites/create.json", arguments.options.merge(:id => id))

lib/twitter/api/utils.rb
@@ -33,9 +33,9 @@ module Twitter
       # @param path [String]
       # @param args [Array]
       # @return [Array<Twitter::User>]
-      def threaded_user_objects_from_response(request_method, path, args)
+      def parallel_user_objects_from_response(request_method, path, args)
         arguments = Twitter::API::Arguments.new(args)
-        arguments.flatten.threaded_map do |user|
+        arguments.flatten.pmap do |user|
           object_from_response(Twitter::User, request_method, path, merge_user(arguments.options, user))
         end
       end
@@ -85,9 +85,9 @@ module Twitter
       # @param path [String]
       # @param args [Array]
       # @return [Array]
-      def threaded_object_from_response(klass, request_method, path, args)
+      def parallel_object_from_response(klass, request_method, path, args)
         arguments = Twitter::API::Arguments.new(args)
-        arguments.flatten.threaded_map do |object|
+        arguments.flatten.pmap do |object|
           id = extract_id(object)
           object_from_response(klass, request_method, path, arguments.options.merge(:id => id))
         end