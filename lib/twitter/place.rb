3e3b2341cca88b90e7aa937e1097bddc81f9b6da
Erik Michaels-Ober <sferik@gmail.com>
2013-09-24 13:33:03 +0200

    Make Twitter::Place inhert directly from Twitter::Base

--- lib/twitter/place.rb
+++ lib/twitter/place.rb
@@ -1,9 +1,12 @@
-require 'twitter/identity'
+require 'equalizer'
+require 'twitter/base'
 
 module Twitter
-  class Place < Twitter::Identity
+  class Place < Twitter::Base
+    include Equalizer.new(:woeid)
     attr_reader :attributes, :country, :full_name, :name, :woeid
     alias woe_id woeid
+    alias id woeid
     object_attr_reader :GeoCreator, :bounding_box
     object_attr_reader :Place, :contained_within
     alias contained? contained_within?
@@ -22,11 +25,6 @@ module Twitter
     # @return [String]
     def place_type
       @attrs[:place_type] || @attrs[:placeType] && @attrs[:placeType][:name]
-    end
-
-    # return [Integer]
-    def id
-      @attrs[:id] || @attrs[:woeid]
     end
 
   end
