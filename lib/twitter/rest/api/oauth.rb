d2239fa6c17848b4d1a00ca88c42c5573cab8d8f
Yannick Schutz <yannick.schutz@gmail.com>
2013-09-29 19:23:50 +0200

    Add json error parser to oauth reverse token

--- lib/twitter/rest/api/oauth.rb
+++ lib/twitter/rest/api/oauth.rb
@@ -1,5 +1,6 @@
 require 'twitter/rest/api/utils'
 require 'twitter/token'
+require 'twitter/rest/response/parse_error_json'
 
 module Twitter
   module REST
@@ -41,7 +42,7 @@ module Twitter
 
         def reverse_token
           conn = connection.dup
-          conn.builder.delete Twitter::REST::Response::ParseJson
+          conn.builder.swap 4, Twitter::REST::Response::ParseErrorJson
           conn.post('/oauth/request_token?x_auth_mode=reverse_auth') do |request|
             request.headers[:authorization] = oauth_auth_header(:post, 'https://api.twitter.com/oauth/request_token', :x_auth_mode => 'reverse_auth').to_s
           end.body
