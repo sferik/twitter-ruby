3770136036dc9e2df43028f1478daad164454efd
eric.vautour <eric.vautour@kineticsocial.com>
2013-11-12 14:44:14 -0500

    Added detection of split token

--- lib/twitter/streaming/response.rb
+++ lib/twitter/streaming/response.rb
@@ -23,6 +23,11 @@ module Twitter
       end
 
       def on_body(data)
+        # Ended with half a token \r and not \r\n and thus BufferedTokenizer will
+        # not split the line correctly.  We need to ensure that each chunk of data passed
+        # to BufferedTokenizer does NOT end with \r
+        data += "\n" if data[-1] == "\r"
+
         @tokenizer.extract(data).each do |line|
           next if line.empty?
           @block.call(JSON.parse(line, :symbolize_names => true))
