79c76a9bed4f0170c8c09fe38ad4f0ee6aa4505e
Erik Michaels-Ober <sferik@gmail.com>
2013-11-28 15:25:56 +0100

    Custom equalizer for Twitter::Place

--- lib/twitter/place.rb
+++ lib/twitter/place.rb
@@ -3,13 +3,25 @@ require 'twitter/base'
 
 module Twitter
   class Place < Twitter::Base
-    include Equalizer.new(:woeid)
     attr_reader :attributes, :country, :full_name, :name, :woeid
     alias woe_id woeid
     object_attr_reader :GeoFactory, :bounding_box
     object_attr_reader :Place, :contained_within
     alias contained? contained_within?
     uri_attr_reader :uri
+
+    # @param other [Twitter::Place]
+    # @return [Boolean]
+    def eql?(other)
+      super || instance_of?(other.class) && !woeid.nil? && other.respond_to?(:woeid) && woeid.eql?(other.woeid)
+    end
+
+    # @param other [Twitter::Place]
+    # @return [Boolean]
+    def ==(other)
+      other = coerce(other) if respond_to?(:coerce, true)
+      super || kind_of?(self.class) && !woeid.nil? && other.respond_to?(:woeid) && woeid == other.woeid
+    end
 
     # @return [String]
     def country_code
