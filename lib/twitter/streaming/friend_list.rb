1a38e5e8182823c3060fc59c270ef754bd49a179
Erik Michaels-Ober <sferik@gmail.com>
2013-11-29 09:50:53 +0100

    Make Twitter::Streaming::FriendList an array

--- lib/twitter/streaming/friend_list.rb
+++ lib/twitter/streaming/friend_list.rb
@@ -1,13 +1,6 @@
 module Twitter
   module Streaming
-    class FriendList
-
-      attr_reader :friend_ids
-
-      # @param friend_ids [Array]
-      def initialize(friend_ids)
-        @friend_ids = friend_ids
-      end
+    class FriendList < Array
     end
   end
 end
