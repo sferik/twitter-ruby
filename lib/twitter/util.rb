c01ea8309c16eb77eeb368452df1dadd1e405532
Erik Michaels-Ober <sferik@gmail.com>
2013-12-18 07:47:03 -0500

    Don't bother spawning a new thread if there's only one item

--- lib/twitter/util.rb
+++ lib/twitter/util.rb
@@ -1,9 +1,12 @@
 module Twitter
   module Util
     def self.threaded_map(enumerable)
-      enumerable.map { |object|
-        Thread.new { yield object }
-      }.map(&:value)
+      # Don't bother spawning a new thread if there's only one item
+      if enumerable.count == 1
+        enumerable.map { |object| yield object }
+      else
+        enumerable.map { |object| Thread.new { yield object } }.map(&:value)
+      end
     end
   end
 end
