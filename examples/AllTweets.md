8e846e1d2d0db40fc24db3247b4e239a30bf0760
Erik Michaels-Ober <sferik@gmail.com>
2013-12-24 12:08:15 -0500

    Allow search query to be passed in options hash

--- examples/AllTweets.md
+++ examples/AllTweets.md
@@ -1,12 +1,13 @@
 # All Tweets
 
-You can fetch up to 3,200 tweets for a user, 200 at a time (up to 16 HTTP
-requests). **Note: This may result in [rate limiting][].**
+You can get up to 3,200 tweets for a user, 200 at a time.
+
+Here is an example of recursively getting pages of 200 Tweets until you receive
+an empty response.
+
+**Note: This may result in [rate limiting][].**
 
 [rate limiting]: https://github.com/sferik/twitter/blob/master/examples/RateLimiting.md
-
-Here is an example of recursively fetching pages of 200 Tweets until you
-receive an empty response.
 
 ```ruby
 require 'twitter'
@@ -17,7 +18,7 @@ def collect_with_max_id(collection=[], max_id=nil, &block)
   response.empty? ? collection.flatten : collect_with_max_id(collection, response.last.id - 1, &block)
 end
 
-def fetch_all_tweets(user)
+def get_all_tweets(user)
   collect_with_max_id do |max_id|
     options = {:count => 200, :include_rts => true}
     options[:max_id] = max_id unless max_id.nil?
@@ -25,5 +26,5 @@ def fetch_all_tweets(user)
   end
 end
 
-fetch_all_tweets("sferik")
+get_all_tweets("sferik")
 ```
--- spec/twitter/rest/api/search_spec.rb
+++ spec/twitter/rest/api/search_spec.rb
@@ -10,26 +10,38 @@ describe Twitter::REST::API::Search do
     before do
       stub_get('/1.1/search/tweets.json').with(:query => {:q => 'twitter'}).to_return(:body => fixture('search.json'), :headers => {:content_type => 'application/json; charset=utf-8'})
     end
-    it 'requests the correct resource' do
-      @client.search('twitter')
-      expect(a_get('/1.1/search/tweets.json').with(:query => {:q => 'twitter'})).to have_been_made
+    context 'with query passed as the first argument' do
+      it 'requests the correct resource' do
+        @client.search('twitter')
+        expect(a_get('/1.1/search/tweets.json').with(:query => {:q => 'twitter'})).to have_been_made
+      end
+      it 'returns recent Tweets related to a query with images and videos embedded' do
+        search = @client.search('twitter')
+        expect(search).to be_a Twitter::SearchResults
+        expect(search.first).to be_a Twitter::Tweet
+        expect(search.first.text).to eq('Bubble Mailer #freebandnames')
+      end
+      it 'returns the max_id value for a search result' do
+        search = @client.search('twitter')
+        expect(search.max_id).to eq(250_126_199_840_518_145)
+      end
     end
-    it 'returns recent Tweets related to a query with images and videos embedded' do
-      search = @client.search('twitter')
-      expect(search).to be_a Twitter::SearchResults
-      expect(search.first).to be_a Twitter::Tweet
-      expect(search.first.text).to eq('Bubble Mailer #freebandnames')
+    context 'with query passed in options hash' do
+      it 'requests the correct resource' do
+        @client.search(:q => 'twitter')
+        expect(a_get('/1.1/search/tweets.json').with(:query => {:q => 'twitter'})).to have_been_made
+      end
+      it 'returns recent Tweets related to a query with images and videos embedded' do
+        search = @client.search(:q => 'twitter')
+        expect(search).to be_a Twitter::SearchResults
+        expect(search.first).to be_a Twitter::Tweet
+        expect(search.first.text).to eq('Bubble Mailer #freebandnames')
+      end
     end
-    it 'returns the max_id value for a search result' do
-      search = @client.search('twitter')
-      expect(search.max_id).to eq(250_126_199_840_518_145)
-    end
-
     context 'when search API responds a malformed result' do
       before do
         stub_get('/1.1/search/tweets.json').with(:query => {:q => 'twitter'}).to_return(:body => fixture('search_malformed.json'), :headers => {:content_type => 'application/json; charset=utf-8'})
       end
-
       it 'returns an empty array' do
         search = @client.search('twitter')
         expect(search.to_a).to be_an Array
