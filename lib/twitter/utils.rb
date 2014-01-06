28c5b736f4f62da44ba80e6c4f851d3cb50c07ed
Erik Michaels-Ober <sferik@gmail.com>
2014-01-06 02:34:05 +0100

    Prefer module function to class << self

--- lib/twitter/utils.rb
+++ lib/twitter/utils.rb
@@ -1,13 +1,12 @@
 module Twitter
   module Utils
-    class << self
-      def parallel_map(enumerable)
-        # Don't bother spawning a new thread if there's only one item
-        if enumerable.count == 1
-          enumerable.map { |object| yield object }
-        else
-          enumerable.map { |object| Thread.new { yield object } }.map(&:value)
-        end
+    module_function
+    def parallel_map(enumerable)
+      # Don't bother spawning a new thread if there's only one item
+      if enumerable.count == 1
+        enumerable.map { |object| yield object }
+      else
+        enumerable.map { |object| Thread.new { yield object } }.map(&:value)
       end
     end
   end
