0cb7e6c132b73766bc978ccb45dd8d9cba43f660
Erik Michaels-Ober <sferik@gmail.com>
2014-02-28 14:32:49 -0800

    Add SSL_REQUIRED error code
    https://www.apichangelog.com/changes/75488a8b-e7ec-424d-96cd-8ff4bb8a532c

--- lib/twitter/error.rb
+++ lib/twitter/error.rb
@@ -7,13 +7,14 @@ module Twitter
 
     # If error code is missing see https://dev.twitter.com/docs/error-codes-responses
     module Code
-      AUTHENTICATION_PROBLEM       = 32
-      RESOURCE_NOT_FOUND           = 34
-      SUSPENDED_ACCOUNT            = 64
-      DEPRECATED_CALL              = 68
-      RATE_LIMIT_EXCEEDED          = 88
-      INVALID_OR_EXPIRED_TOKEN     = 89
-      UNABLE_TO_VERIFY_CREDENTIALS = 99
+      AUTHENTICATION_PROBLEM       =  32
+      RESOURCE_NOT_FOUND           =  34
+      SUSPENDED_ACCOUNT            =  64
+      DEPRECATED_CALL              =  68
+      RATE_LIMIT_EXCEEDED          =  88
+      INVALID_OR_EXPIRED_TOKEN     =  89
+      SSL_REQUIRED                 =  92
+      UNABLE_TO_VERIFY_CREDENTIALS =  99
       OVER_CAPACITY                = 130
       INTERNAL_ERROR               = 131
       OAUTH_TIMESTAMP_OUT_OF_RANGE = 135
