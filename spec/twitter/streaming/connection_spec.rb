237cb81909a14a1737b51989226a37d9d567968d
Nikica Jokic <neektza@gmail.com>
2014-03-24 00:36:22 +0100

    RuboCop's suggestions implemented.

--- spec/twitter/streaming/connection_spec.rb
+++ spec/twitter/streaming/connection_spec.rb
@@ -3,32 +3,30 @@ require 'helper'
 describe Twitter::Streaming::Connection do
 
   describe 'initialize' do
-
-    context "no options provided" do
+    context 'no options provided' do
       subject(:connection) { Twitter::Streaming::Connection.new }
 
-      it "sets the default socket classes" do
+      it 'sets the default socket classes' do
         expect(connection.tcp_socket_klass).to eq TCPSocket
         expect(connection.ssl_socket_klass).to eq OpenSSL::SSL::SSLSocket
       end
     end
 
-    context "custom socket classes provided in opts" do
+    context 'custom socket classes provided in opts' do
       class DummyTCPSocket; end
       class DummySSLSocket; end
 
-      subject(:connection) { Twitter::Streaming::Connection.new(
-        tcp_socket_klass: DummyTCPSocket,
-        ssl_socket_klass: DummySSLSocket
-      )}
+      subject(:connection) do
+        Twitter::Streaming::Connection.new(
+          :tcp_socket_klass => DummyTCPSocket,
+          :ssl_socket_klass => DummySSLSocket
+        )
+      end
 
-      it "sets the default socket classes" do
+      it 'sets the default socket classes' do
         expect(connection.tcp_socket_klass).to eq DummyTCPSocket
         expect(connection.ssl_socket_klass).to eq DummySSLSocket
       end
     end
-
   end
-
 end
-
