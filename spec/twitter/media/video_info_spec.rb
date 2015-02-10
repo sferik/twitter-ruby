e04358fc966e500d4f4e6cf51e61cbcb1800fac5
Nidev Plontra <nidev.plontra@gmail.com>
2015-02-10 20:25:03 +0900

    Follow code style of 5-stable branch. Disable 'Style/EmptyLinesAroundBlockBody' on .rubocop.yml due to many spec files written in that style.

--- spec/twitter/media/video_info_spec.rb
+++ spec/twitter/media/video_info_spec.rb
@@ -3,7 +3,7 @@ require 'helper'
 describe Twitter::Media::VideoInfo do
   describe '#aspect_ratio' do
     it 'returns a String when the aspect_ratio is set' do
-      info = Twitter::Media::VideoInfo.new(aspect_ratio: [16, 9])
+      info = Twitter::Media::VideoInfo.new(:aspect_ratio => [16, 9])
       expect(info.aspect_ratio).to be_an Array
       expect(info.aspect_ratio).to eq([16, 9])
     end
@@ -15,7 +15,7 @@ describe Twitter::Media::VideoInfo do
 
   describe '#duration_millis' do
     it 'returns a Integer when the duration_millis is set' do
-      info = Twitter::Media::VideoInfo.new(duration_millis: 30_033)
+      info = Twitter::Media::VideoInfo.new(:duration_millis => 30_033)
       expect(info.duration_millis).to be_a Integer
       expect(info.duration_millis).to eq(30_033)
     end
@@ -27,7 +27,7 @@ describe Twitter::Media::VideoInfo do
 
   describe '#variants' do
     it 'returns a hash of Variants when variants is set' do
-      variants = Twitter::Media::VideoInfo.new(variants: [{bitrate: 2_176_000, content_type: 'video/mp4', url: 'http://video.twimg.com/c4E56sl91ZB7cpYi.mp4'}]).variants
+      variants = Twitter::Media::VideoInfo.new(:variants => [{:bitrate => 2_176_000, :content_type => 'video/mp4', :url => 'http://video.twimg.com/c4E56sl91ZB7cpYi.mp4'}]).variants
       expect(variants).to be_an Array
       expect(variants.first).to be_a Twitter::Variant
     end
