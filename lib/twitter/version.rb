dc314cd92624bdada5b651b27baf238b2cbe0640
Erik Michaels-Ober <sferik@gmail.com>
2015-02-24 05:59:56 +0100

    Convert Twitter::Version class to a module

--- lib/twitter/version.rb
+++ lib/twitter/version.rb
@@ -1,45 +1,45 @@
 module Twitter
-  class Version
-    class << self
-      # @return [Integer]
-      def major
-        5
-      end
+  module Version
+  module_function
 
-      # @return [Integer]
-      def minor
-        14
-      end
+    # @return [Integer]
+    def major
+      5
+    end
 
-      # @return [Integer]
-      def patch
-        0
-      end
+    # @return [Integer]
+    def minor
+      14
+    end
 
-      # @return [Integer, NilClass]
-      def pre
-        nil
-      end
+    # @return [Integer]
+    def patch
+      0
+    end
 
-      # @return [Hash]
-      def to_h
-        {
-          :major => MAJOR,
-          :minor => MINOR,
-          :patch => PATCH,
-          :pre => PRE,
-        }
-      end
+    # @return [Integer, NilClass]
+    def pre
+      nil
+    end
 
-      # @return [Array]
-      def to_a
-        to_h.values.compact
-      end
+    # @return [Hash]
+    def to_h
+      {
+        :major => major,
+        :minor => minor,
+        :patch => patch,
+        :pre => pre,
+      }
+    end
 
-      # @return [String]
-      def to_s
-        to_a.join('.')
-      end
+    # @return [Array]
+    def to_a
+      to_h.values.compact
+    end
+
+    # @return [String]
+    def to_s
+      to_a.join('.')
     end
   end
 end
