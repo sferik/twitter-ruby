db16739659def16e1992c8da5bf7efea2d1486dd
Nidev Plontra <nidev.plontra@gmail.com>
2015-03-18 22:39:56 +0900

    Also rescue from Errno::EAGAIN, to keep compatibility with 1.8.x

--- lib/twitter/streaming/connection.rb
+++ lib/twitter/streaming/connection.rb
@@ -23,7 +23,7 @@ module Twitter
           begin
             body = ssl_client.read_nonblock(1024)
             response << body
-          rescue IO::WaitReadable
+          rescue IO::WaitReadable, Errno::EAGAIN
             readables, _, _ = IO.select([ssl_client], [], [], @select_timeout)
             if readables.nil?
               ssl_client.close
