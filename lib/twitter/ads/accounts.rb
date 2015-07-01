26dee741e81fd7c7c6a3cbdc679ec124e6b7069d
Thomas Holmes <thomas@holmes.io>
2015-07-01 18:20:38 -0400

    Add scoped_timeline endpoint.

--- lib/twitter/ads/accounts.rb
+++ lib/twitter/ads/accounts.rb
@@ -1,9 +1,10 @@
 require 'twitter/account'
+require 'twitter/ads/utils'
 require 'twitter/arguments'
 require 'twitter/error'
 require 'twitter/rest/request'
-require 'twitter/ads/utils'
 require 'twitter/settings'
+require 'twitter/tweet'
 require 'twitter/utils'
 
 module Twitter
@@ -37,6 +38,27 @@ module Twitter
       # @option options [Boolean] :with_deleted Set to true if you want deleted accounts to be returned.
       def account(id, options = {})
         perform_get_with_object("https://ads-api.twitter.com/0/accounts/#{id}", options, Twitter::Account)
+      end
+
+      # Retrieve up to 200 of the most recent promotable Tweets created by one or more
+      # specified Twitter users.
+      #
+      # @see https://dev.twitter.com/ads/reference/get/accounts/%3Aaccount_id/scoped_timeline
+      # @rate_limited Yes
+      # @authentication Requires user context
+      # @raise [Twitter::Error::Unauthorized] Error raised when supplied user credentials are not valid.
+      # @return [Array<Twitter::Tweet>]
+      # @param account_id [String] Ads account id.
+      # @param user_ids [Array<Integer>, Array<String>, String, Integer] Array of twitter user ids or comma separated string of ids
+      # @param options [Hash] customizeable options.
+      # @option options [String] :scoped_to Scope search. See documentation for options.
+      # @option options [String] :objective Objective type to restrict timeline to.
+      # @option options [Boolean] :trim_user set to true to only receive user id rather than the full profile.
+      def scoped_timeline(id, user_ids, options = {})
+        ids = [user_ids].flatten.join(',')
+        options = options.merge(user_ids: ids)
+        perform_get_with_cursor("https://ads-api.twitter.com/0/accounts/#{id}/scoped_timeline", options,
+                                :data, Twitter::Tweet)
       end
     end
   end
--- spec/twitter/ads/accounts_spec.rb
+++ spec/twitter/ads/accounts_spec.rb
@@ -30,4 +30,15 @@ describe Twitter::Ads::Accounts do
       expect(account.id).to eq('gq0vll')
     end
   end
+
+  describe '#scoped_timeline' do
+    before do
+      stub_get('https://ads-api.twitter.com/0/accounts/abc123/scoped_timeline').with(query: { user_ids: 783214 }).to_return(body: fixture('scoped_timeline.json'), headers: {content_type: 'application/json; charset=utf-8'})
+    end
+    it 'requests the correct resource' do
+      tweets = @client.scoped_timeline('abc123', 783214)
+      expect(tweets.count).to eq(1)
+      expect(tweets.first.id).to eq(288524949692489728)
+    end
+  end
 end
