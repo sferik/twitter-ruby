c63d5b47e1050185e153a9a19e66e0bae460d1b0
Thomas Holmes <thomas@holmes.io>
2015-08-12 13:55:32 -0400

    Support all stats endpoints.
    Unfortunately this hides the (fairly important) rate-limiting
    information that is contained in the response headers.

--- lib/twitter/ads/statistics.rb
+++ lib/twitter/ads/statistics.rb
@@ -1,6 +1,8 @@
-require 'twitter/stats'
-require 'twitter/rest/request'
 require 'twitter/ads/utils'
+require 'twitter/error'
+require 'twitter/rest/request'
+require 'twitter/settings'
+require 'twitter/stats'
 require 'twitter/utils'
 
 module Twitter
@@ -26,7 +28,7 @@ module Twitter
       # @option options [String] :country A country targeting_value required for certain segmentation types.
       # @option options [String] :platform A platform targeting_value required for certain segmentation types.
       def account_stats(account_id, options = {})
-        raise 'Not yet implemented'
+        perform_get_for_stats("https://ads-api.twitter.com/0/stats/accounts/#{account_id}", options)
       end
 
       # Returns a stats specific to a given campaign.
@@ -47,8 +49,7 @@ module Twitter
       # @option options [String] :country A country targeting_value required for certain segmentation types.
       # @option options [String] :platform A platform targeting_value required for certain segmentation types.
       def campaign_stats(account_id, campaign_id, options = {})
-        perform_get_for_stats("https://ads-api.twitter.com/0/stats/accounts/#{account_id}/campaigns/#{campaign_id}",
-                                options, Twitter::Stats)
+        perform_get_for_stats("https://ads-api.twitter.com/0/stats/accounts/#{account_id}/campaigns/#{campaign_id}", options)
       end
 
       # Returns a stats specific to a given line item.
@@ -69,7 +70,7 @@ module Twitter
       # @option options [String] :country A country targeting_value required for certain segmentation types.
       # @option options [String] :platform A platform targeting_value required for certain segmentation types.
       def line_item_stats(account_id, line_item_id, options = {})
-        raise 'Not yet implemented'
+        perform_get_for_stats("https://ads-api.twitter.com/0/stats/accounts/#{account_id}/line_items/#{line_item_id}", options)
       end
 
       # Returns a stats specific to a given promoted tweet.
@@ -90,7 +91,7 @@ module Twitter
       # @option options [String] :country A country targeting_value required for certain segmentation types.
       # @option options [String] :platform A platform targeting_value required for certain segmentation types.
       def promoted_tweet_stats(account_id, promoted_tweet_id, options = {})
-        raise 'Not yet implemented'
+        perform_get_for_stats("https://ads-api.twitter.com/0/stats/accounts/#{account_id}/promoted_tweets/#{promoted_tweet_id}", options)
       end
 
       # Returns a stats specific to a given promoted account.
@@ -111,7 +112,7 @@ module Twitter
       # @option options [String] :country A country targeting_value required for certain segmentation types.
       # @option options [String] :platform A platform targeting_value required for certain segmentation types.
       def promoted_account_stats(account_id, promoted_account_id, options = {})
-        raise 'Not yet implemented'
+        perform_get_for_stats("https://ads-api.twitter.com/0/stats/accounts/#{account_id}/promoted_accounts/#{promoted_account_id}", options)
       end
 
       # Returns a stats specific to a given funding instrument.
@@ -132,7 +133,7 @@ module Twitter
       # @option options [String] :country A country targeting_value required for certain segmentation types.
       # @option options [String] :platform A platform targeting_value required for certain segmentation types.
       def funding_instrument_stats(account_id, funding_instrument_id, options = {})
-        raise 'Not yet implemented'
+        perform_get_for_stats("https://ads-api.twitter.com/0/stats/accounts/#{account_id}/funding_instruments/#{funding_instrument_id}", options)
       end
 
       # Return aggregate stats specific to a collection of campaigns
@@ -153,7 +154,8 @@ module Twitter
       # @option options [String] :country A country targeting_value required for certain segmentation types.
       # @option options [String] :platform A platform targeting_value required for certain segmentation types.
       def campaigns_stats(account_id, campaign_ids, options = {})
-        raise 'Not yet implemented'
+        perform_collection_get_for_stats("https://ads-api.twitter.com/0/stats/accounts/#{account_id}/campaigns",
+                                        campaign_ids, :campaign_ids,  options)
       end
 
       # Return aggregate stats specific to a collection of line items.
@@ -174,7 +176,8 @@ module Twitter
       # @option options [String] :country A country targeting_value required for certain segmentation types.
       # @option options [String] :platform A platform targeting_value required for certain segmentation types.
       def line_items_stats(account_id, line_item_ids, options = {})
-        raise 'Not yet implemented'
+        perform_collection_get_for_stats("https://ads-api.twitter.com/0/stats/accounts/#{account_id}/line_items",
+                                        line_item_ids, :line_item_ids,  options)
       end
 
       # Return aggregate stats specific to a collection of promoted tweets.
@@ -195,7 +198,8 @@ module Twitter
       # @option options [String] :country A country targeting_value required for certain segmentation types.
       # @option options [String] :platform A platform targeting_value required for certain segmentation types.
       def promoted_tweets_stats(account_id, promoted_tweet_ids, options = {})
-        raise 'Not yet implemented'
+        perform_collection_get_for_stats("https://ads-api.twitter.com/0/stats/accounts/#{account_id}/promoted_tweets",
+                                        promoted_tweet_ids, :promoted_tweet_ids,  options)
       end
 
       # Return aggregate stats specific to a collection of promoted accounts.
@@ -216,7 +220,8 @@ module Twitter
       # @option options [String] :country A country targeting_value required for certain segmentation types.
       # @option options [String] :platform A platform targeting_value required for certain segmentation types.
       def promoted_accounts_stats(account_id, promoted_account_ids, options = {})
-        raise 'Not yet implemented'
+        perform_collection_get_for_stats("https://ads-api.twitter.com/0/stats/accounts/#{account_id}/promoted_accounts",
+                                        promoted_account_ids, :promoted_account_ids,  options)
       end
 
       # Return aggregate stats specific to a collection of funding instruments
@@ -237,19 +242,29 @@ module Twitter
       # @option options [String] :country A country targeting_value required for certain segmentation types.
       # @option options [String] :platform A platform targeting_value required for certain segmentation types.
       def funding_instruments_stats(account_id, funding_instrument_ids, options = {})
-        raise 'Not yet implemented'
+        perform_collection_get_for_stats("https://ads-api.twitter.com/0/stats/accounts/#{account_id}/funding_instruments",
+                                        funding_instrument_ids, :funding_instrument_ids,  options)
       end
 
     private
 
       # If you elect for segmentation twitter returns an array of stats objects rather
       # than a single stats object.
-      def perform_get_for_stats(url, options, klass)
+      def perform_get_for_stats(url, options, klass = Twitter::Stats)
         if options.has_key?(:segmentation_type) || options.has_key?('segmentation_type')
           perform_get_with_objects(url, options, klass)
         else
           perform_get_with_object(url, options, klass)
         end
+      end
+
+      def perform_collection_get_for_stats(url, ids, key, options, klass = Twitter::Stats)
+        options[key] = case ids
+                       when String then ids
+                       when Array then ids.join(',')
+                       else raise 'Ids must be a string or array of strings.'
+                       end
+        perform_get_with_objects(url, options, klass)
       end
     end
   end
--- spec/twitter/ads/statistics_spec.rb
+++ spec/twitter/ads/statistics_spec.rb
@@ -6,6 +6,22 @@ describe Twitter::Ads::Statistics do
   end
 
   describe '#account_stats' do
+    let(:start_time) { '2012-11-20T07:00:00Z' }
+    before do
+      stub_get('https://ads-api.twitter.com/0/stats/accounts/43853bhii879')
+        .with(query: {granularity: 'TOTAL', start_time: start_time}).to_return(body: fixture('account_stats.json'), headers:{content_type: 'application/json; charset=utf-8'})
+    end
+
+    it 'requests resources' do
+      @client.account_stats('43853bhii879', start_time: start_time, granularity: 'TOTAL')
+      expect(a_get('https://ads-api.twitter.com/0/stats/accounts/43853bhii879').with(query: {granularity: 'TOTAL', start_time: start_time})).to have_been_made
+    end
+
+    it 'gets the right resource' do
+      stats = @client.account_stats('43853bhii879', start_time: start_time, granularity: 'TOTAL')
+      expect(stats.id).to eq('43853bhii879')
+      expect(stats.promoted_tweet_timeline_retweets).to eq([28])
+    end
   end
 
   describe '#campaign_stats' do
@@ -39,29 +55,173 @@ describe Twitter::Ads::Statistics do
   end
 
   describe '#line_item_stats' do
+    let(:start_time) { '2012-11-20T07:00:00Z' }
+    before do
+      stub_get('https://ads-api.twitter.com/0/stats/accounts/hkk5/line_items/5woz')
+        .with(query: {granularity: 'DAY', start_time: start_time}).to_return(body: fixture('line_item_stats.json'), headers:{content_type: 'application/json; charset=utf-8'})
+    end
+
+    it 'requests resources' do
+      @client.line_item_stats('hkk5', '5woz', start_time: start_time, granularity: 'DAY')
+      expect(a_get('https://ads-api.twitter.com/0/stats/accounts/hkk5/line_items/5woz').with(query: {granularity: 'DAY', start_time: start_time})).to have_been_made
+    end
+
+    it 'gets the right resource' do
+      stats = @client.line_item_stats('hkk5', '5woz', start_time: start_time, granularity: 'DAY')
+      expect(stats.id).to eq('5woz')
+      expect(stats.promoted_tweet_timeline_clicks.reduce(:+)).to eq(1128)
+    end
   end
 
   describe '#promoted_tweet_stats' do
+    let(:start_time) { '2012-11-20T07:00:00Z' }
+    before do
+      stub_get('https://ads-api.twitter.com/0/stats/accounts/4cury/promoted_tweets/2m4ky')
+        .with(query: {granularity: 'TOTAL', start_time: start_time}).to_return(body: fixture('promoted_tweet_stats.json'), headers:{content_type: 'application/json; charset=utf-8'})
+    end
+
+    it 'requests resources' do
+      @client.promoted_tweet_stats('4cury', '2m4ky', start_time: start_time, granularity: 'TOTAL')
+      expect(a_get('https://ads-api.twitter.com/0/stats/accounts/4cury/promoted_tweets/2m4ky').with(query: {granularity: 'TOTAL', start_time: start_time})).to have_been_made
+    end
+
+    it 'gets the right resource' do
+      stats = @client.promoted_tweet_stats('4cury', '2m4ky', start_time: start_time, granularity: 'TOTAL')
+      expect(stats.id).to eq('2m4ky')
+      expect(stats.promoted_tweet_search_clicks).to eq([0])
+    end
   end
 
   describe '#promoted_account_stats' do
+    let(:start_time) { '2013-05-01T00:00:00Z' }
+    before do
+      stub_get('https://ads-api.twitter.com/0/stats/accounts/hkk5/promoted_accounts/d9qr')
+        .with(query: {granularity: 'TOTAL', start_time: start_time}).to_return(body: fixture('promoted_account_stats.json'), headers:{content_type: 'application/json; charset=utf-8'})
+    end
+
+    it 'requests resources' do
+      @client.promoted_account_stats('hkk5', 'd9qr', start_time: start_time, granularity: 'TOTAL')
+      expect(a_get('https://ads-api.twitter.com/0/stats/accounts/hkk5/promoted_accounts/d9qr').with(query: {granularity: 'TOTAL', start_time: start_time})).to have_been_made
+    end
+
+    it 'gets the right resource' do
+      stats = @client.promoted_account_stats('hkk5', 'd9qr', start_time: start_time, granularity: 'TOTAL')
+      expect(stats.id).to eq('d9qr')
+      expect(stats.promoted_account_impressions).to eq([30689])
+    end
   end
 
   describe '#funding_instrument_stats' do
+    let(:start_time) { '2013-04-13T07:00:00Z' }
+    before do
+      stub_get('https://ads-api.twitter.com/0/stats/accounts/5gvk9h/funding_instruments/e25e')
+        .with(query: {granularity: 'DAY', start_time: start_time}).to_return(body: fixture('funding_instrument_stats.json'), headers:{content_type: 'application/json; charset=utf-8'})
+    end
+
+    it 'requests resources' do
+      @client.funding_instrument_stats('5gvk9h', 'e25e', start_time: start_time, granularity: 'DAY')
+      expect(a_get('https://ads-api.twitter.com/0/stats/accounts/5gvk9h/funding_instruments/e25e').with(query: {granularity: 'DAY', start_time: start_time})).to have_been_made
+    end
+
+    it 'gets the right resource' do
+      stats = @client.funding_instrument_stats('5gvk9h', 'e25e', start_time: start_time, granularity: 'DAY')
+      expect(stats.id).to eq('e25e')
+      expect(stats.promoted_tweet_timeline_engagements).to eq([65, 75, 81])
+    end
   end
 
   describe '#campaigns_stats' do
+    let(:start_time) { '2013-04-13T07:00:00Z' }
+    before do
+      stub_get('https://ads-api.twitter.com/0/stats/accounts/5gvk9h/campaigns')
+        .with(query: {granularity: 'DAY', start_time: start_time, campaign_ids: 'e25e'}).to_return(body: fixture('campaigns_stats.json'), headers:{content_type: 'application/json; charset=utf-8'})
+    end
+
+    it 'requests resources' do
+      @client.campaigns_stats('5gvk9h', ['e25e'], start_time: start_time, granularity: 'DAY')
+      expect(a_get('https://ads-api.twitter.com/0/stats/accounts/5gvk9h/campaigns').with(query: {granularity: 'DAY', start_time: start_time, campaign_ids: 'e25e'})).to have_been_made
+    end
+
+    it 'gets the right resource' do
+      stats = @client.campaigns_stats('5gvk9h', ['e25e'], start_time: start_time, granularity: 'DAY')
+      expect(stats.first.id).to eq('e25e')
+      expect(stats.first.promoted_tweet_timeline_engagements).to eq([65, 75, 81])
+    end
   end
 
   describe '#line_items_stats' do
+    let(:start_time) { '2013-04-13T07:00:00Z' }
+    before do
+      stub_get('https://ads-api.twitter.com/0/stats/accounts/hkk5/line_items')
+        .with(query: {granularity: 'DAY', start_time: start_time, line_item_ids: '5woz'}).to_return(body: fixture('line_items_stats.json'), headers:{content_type: 'application/json; charset=utf-8'})
+    end
+
+    it 'requests resources' do
+      @client.line_items_stats('hkk5', ['5woz'], start_time: start_time, granularity: 'DAY')
+      expect(a_get('https://ads-api.twitter.com/0/stats/accounts/hkk5/line_items').with(query: {granularity: 'DAY', start_time: start_time, line_item_ids: '5woz'})).to have_been_made
+    end
+
+    it 'gets the right resource' do
+      stats = @client.line_items_stats('hkk5', ['5woz'], start_time: start_time, granularity: 'DAY')
+      expect(stats.first.id).to eq('5woz')
+      expect(stats.first.promoted_tweet_timeline_clicks.reduce(:+)).to eq(1128)
+    end
   end
 
   describe '#promoted_tweets_stats' do
+    let(:start_time) { '2013-04-01T00:00:00Z' }
+    before do
+      stub_get('https://ads-api.twitter.com/0/stats/accounts/hkk5/promoted_tweets')
+        .with(query: {granularity: 'TOTAL', start_time: start_time, promoted_tweet_ids: 'rd9q'}).to_return(body: fixture('promoted_tweets_stats.json'), headers:{content_type: 'application/json; charset=utf-8'})
+    end
+
+    it 'requests resources' do
+      @client.promoted_tweets_stats('hkk5', ['rd9q'], start_time: start_time, granularity: 'TOTAL')
+      expect(a_get('https://ads-api.twitter.com/0/stats/accounts/hkk5/promoted_tweets').with(query: {granularity: 'TOTAL', start_time: start_time, promoted_tweet_ids: 'rd9q'})).to have_been_made
+    end
+
+    it 'gets the right resource' do
+      stats = @client.promoted_tweets_stats('hkk5', ['rd9q'], start_time: start_time, granularity: 'TOTAL')
+      expect(stats.first.id).to eq('rd9q')
+      expect(stats.first.promoted_account_follows).to eq([59])
+    end
   end
 
   describe '#promoted_accounts_stats' do
+    let(:start_time) { '2013-04-01T00:00:00Z' }
+    before do
+      stub_get('https://ads-api.twitter.com/0/stats/accounts/hkk5/promoted_accounts')
+        .with(query: {granularity: 'TOTAL', start_time: start_time, promoted_account_ids: 'd9qr'}).to_return(body: fixture('promoted_accounts_stats.json'), headers:{content_type: 'application/json; charset=utf-8'})
+    end
+
+    it 'requests resources' do
+      @client.promoted_accounts_stats('hkk5', ['d9qr'], start_time: start_time, granularity: 'TOTAL')
+      expect(a_get('https://ads-api.twitter.com/0/stats/accounts/hkk5/promoted_accounts').with(query: {granularity: 'TOTAL', start_time: start_time, promoted_account_ids: 'd9qr'})).to have_been_made
+    end
+
+    it 'gets the right resource' do
+      stats = @client.promoted_accounts_stats('hkk5', ['d9qr'], start_time: start_time, granularity: 'TOTAL')
+      expect(stats.first.id).to eq('d9qr')
+      expect(stats.first.promoted_account_follows).to eq([59])
+    end
   end
 
   describe '#funding_instruments_stats' do
+    let(:start_time) { '2013-04-01T00:00:00Z' }
+    before do
+      stub_get('https://ads-api.twitter.com/0/stats/accounts/5gvk9h/funding_instruments')
+        .with(query: {granularity: 'DAY', start_time: start_time, funding_instrument_ids: 'e25e'}).to_return(body: fixture('funding_instruments_stats.json'), headers:{content_type: 'application/json; charset=utf-8'})
+    end
+
+    it 'requests resources' do
+      @client.funding_instruments_stats('5gvk9h', ['e25e'], start_time: start_time, granularity: 'DAY')
+      expect(a_get('https://ads-api.twitter.com/0/stats/accounts/5gvk9h/funding_instruments').with(query: {granularity: 'DAY', start_time: start_time, funding_instrument_ids: 'e25e'})).to have_been_made
+    end
+
+    it 'gets the right resource' do
+      stats = @client.funding_instruments_stats('5gvk9h', ['e25e'], start_time: start_time, granularity: 'DAY')
+      expect(stats.first.id).to eq('e25e')
+      expect(stats.first.promoted_tweet_timeline_clicks).to eq([65, 75, 81])
+    end
   end
 end
