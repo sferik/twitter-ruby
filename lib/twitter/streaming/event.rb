5049d1fcb912fa1261ea1897b24450c894250695
Andrey Shmelkov <irratio@gmail.com>
2016-02-20 03:09:31 +0500

    Twitter::Streaming::Event refactored: now it descends from Twitter::Base and mixins Twitter::Creatable for created_at support. favorited_retweet and retweeted_retweet are added to the list of tweet events

--- lib/twitter/streaming/event.rb
+++ lib/twitter/streaming/event.rb
@@ -1,35 +1,44 @@
+require 'twitter/base'
+require 'twitter/creatable'
+
 module Twitter
   module Streaming
-    class Event
+    class Event < Twitter::Base
+      include Twitter::Creatable
+
       LIST_EVENTS = [
-        :list_created, :list_destroyed, :list_updated, :list_member_added,
-        :list_member_added, :list_member_removed, :list_user_subscribed,
-        :list_user_subscribed, :list_user_unsubscribed, :list_user_unsubscribed
+        :list_created, :list_destroyed, :list_updated,
+        :list_member_added, :list_member_removed,
+        :list_user_subscribed, :list_user_unsubscribed
       ].freeze
 
       TWEET_EVENTS = [
-        :favorite, :unfavorite
+        :favorite, :unfavorite, :favorited_retweet, :retweeted_retweet
       ].freeze
 
-      attr_reader :name, :source, :target, :target_object
+      object_attr_reader :User, :source
+      object_attr_reader :User, :target
+      define_predicate_method :target_object
 
-      # @param data [Hash]
-      def initialize(data)
-        @name = data[:event].to_sym
-        @source = Twitter::User.new(data[:source])
-        @target = Twitter::User.new(data[:target])
-        @target_object = target_object_factory(@name, data[:target_object])
-      end
+      # @return [Twitter::List, Twitter::Tweet, Twitter::NullObject]
+      def target_object
+        data = @attrs[:target_object]
 
-    private
-
-      def target_object_factory(event_name, data)
-        if LIST_EVENTS.include?(event_name)
+        if LIST_EVENTS.include?(name)
           Twitter::List.new(data)
-        elsif TWEET_EVENTS.include?(event_name)
+        elsif TWEET_EVENTS.include?(name)
           Twitter::Tweet.new(data)
+        else
+          Twitter::NullObject.new
         end
       end
+      memoize :target_object
+
+      # @return [Symbol]
+      def name
+        @attrs.fetch(:event, :'').to_sym
+      end
+      memoize :name
     end
   end
 end
