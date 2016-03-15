47d1af4d9dd6470adcfc95513b2d91c45a3e1168
stve <steve.agalloco@gmail.com>
2016-03-14 22:17:41 -0400

    fix class reference in AnimatedGif spec

--- spec/twitter/media/animated_gif_spec.rb
+++ spec/twitter/media/animated_gif_spec.rb
@@ -5,12 +5,12 @@ describe Twitter::Media::AnimatedGif do
 
   describe '#video_info' do
     it 'returns a Twitter::Media::VideoInfo when the video is set' do
-      video = Twitter::Media::Video.new(id: 1, video_info: {})
-      expect(video.video_info).to be_a Twitter::Media::VideoInfo
+      image = Twitter::Media::AnimatedGif.new(id: 1, video_info: {})
+      expect(image.video_info).to be_a Twitter::Media::VideoInfo
     end
     it 'returns nil when the display_url is not set' do
-      video = Twitter::Media::Video.new(id: 1, video_info: nil)
-      expect(video.video_info).to be_nil
+      image = Twitter::Media::AnimatedGif.new(id: 1, video_info: nil)
+      expect(image.video_info).to be_nil
     end
   end
 end
