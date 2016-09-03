caba8ca595ae715e467a4baec921699ab1add17b
Code Ass <aycabta@gmail.com>
2016-09-03 23:04:54 +0900

    Use hash rocket

--- lib/twitter/rest/tweets.rb
+++ lib/twitter/rest/tweets.rb
@@ -229,7 +229,7 @@ module Twitter
         media_ids = pmap(array_wrap(media)) do |medium|
           upload(medium)[:media_id]
         end
-        update!(status, options.merge(media_ids: media_ids.join(',')))
+        update!(status, options.merge(:media_ids => media_ids.join(',')))
       end
 
       # Returns oEmbed for a Tweet
@@ -307,28 +307,28 @@ module Twitter
       # @see https://dev.twitter.com/rest/public/uploading-media
       def upload(media) # rubocop:disable MethodLength
         if !(File.basename(media) =~ /\.mp4$/)
-          Twitter::REST::Request.new(self, :multipart_post, 'https://upload.twitter.com/1.1/media/upload.json', key: :media, file: media).perform
+          Twitter::REST::Request.new(self, :multipart_post, 'https://upload.twitter.com/1.1/media/upload.json', :key => :media, :file => media).perform
         else
           init = Twitter::REST::Request.new(self, :post, 'https://upload.twitter.com/1.1/media/upload.json',
-                                            command: 'INIT',
-                                            media_type: 'video/mp4',
-                                            total_bytes: media.size).perform
+                                            :command => 'INIT',
+                                            :media_type => 'video/mp4',
+                                            :total_bytes => media.size).perform
 
           until media.eof?
             chunk = media.read(5_000_000)
             seg ||= -1
             Twitter::REST::Request.new(self, :multipart_post, 'https://upload.twitter.com/1.1/media/upload.json',
-                                       command: 'APPEND',
-                                       media_id: init[:media_id],
-                                       segment_index: seg += 1,
-                                       key: :media,
-                                       file: StringIO.new(chunk)).perform
+                                       :command => 'APPEND',
+                                       :media_id => init[:media_id],
+                                       :segment_index => seg += 1,
+                                       :key => :media,
+                                       :file => StringIO.new(chunk)).perform
           end
 
           media.close
 
           Twitter::REST::Request.new(self, :post, 'https://upload.twitter.com/1.1/media/upload.json',
-                                     command: 'FINALIZE', media_id: init[:media_id]).perform
+                                     :command => 'FINALIZE', :media_id => init[:media_id]).perform
         end
       end
 
