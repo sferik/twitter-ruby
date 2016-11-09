ad309d68abc3514b446d140b8bf32fa215eeedfc
Benny Klotz <benny.klotz92@gmail.com>
2016-11-09 16:48:27 +0100

    Use http's timeout api
    Currently twitter gem users can not set a timeout for requests which could lead to stuck requests.
    This commit implements the usage of https://github.com/httprb/http/wiki/Timeouts.
    
    Also this fixes #791.

--- spec/twitter/rest/request_spec.rb
+++ spec/twitter/rest/request_spec.rb
@@ -34,6 +34,28 @@ describe Twitter::REST::Request do
         expect(HTTP).to receive(:via).with('127.0.0.1', 3328).twice.and_call_original
         @client.update_with_media('Update', fixture('pbjt.gif'))
       end
+
+      context 'when using timeout options' do
+        before do
+          @client = Twitter::REST::Client.new(consumer_key: 'CK', consumer_secret: 'CS', access_token: 'AT', access_token_secret: 'AS', proxy: {host: '127.0.0.1', port: 3328}, timeouts: { connect: 2, read: 2, write: 3 })
+        end
+        it 'requests with given timeout settings' do
+          stub_post('/1.1/statuses/update.json').with(body: {status: 'Update'}).to_return(body: fixture('status.json'), headers: {content_type: 'application/json; charset=utf-8'})
+          expect_any_instance_of(HTTP::Client).to receive(:timeout).with(:per_operation, connect: 2, read: 2, write: 3).and_call_original
+          @client.update('Update')
+        end
+      end
+    end
+
+    context 'when using timeout options' do
+      before do
+        @client = Twitter::REST::Client.new(consumer_key: 'CK', consumer_secret: 'CS', access_token: 'AT', access_token_secret: 'AS', timeouts: { connect: 2, read: 2, write: 3 })
+      end
+      it 'requests with given timeout settings' do
+        stub_post('/1.1/statuses/update.json').with(body: {status: 'Update'}).to_return(body: fixture('status.json'), headers: {content_type: 'application/json; charset=utf-8'})
+        expect(HTTP).to receive(:timeout).with(:per_operation, connect: 2, read: 2, write: 3).and_call_original
+        @client.update('Update')
+      end
     end
   end
 end
