d1aa619d2ab648d731f544cfda57662685ec10d2
Daniel Cukier <tech+daniel@playax.com>
2017-04-25 11:24:29 -0300

    build direct message object on direct message event

--- lib/twitter/direct_message_event.rb
+++ lib/twitter/direct_message_event.rb
@@ -6,11 +6,32 @@ module Twitter
   class DirectMessageEvent < Twitter::Identity
     include Twitter::Creatable
     include Twitter::Entities
-    # @return [String]
+
     attr_reader :created_timestamp
-    attr_reader :recipient_id
-    attr_reader :sender_id
-    attr_reader :text
+
+    object_attr_reader :DirectMessage, :direct_message
+
+    def initialize(attrs)
+      text = attrs[:message_create][:message_data][:text]
+      urls = attrs[:message_create][:message_data][:entities][:urls]
+
+      if urls.any?
+        text.gsub!(
+          urls[0][:url],
+          urls[0][:expanded_url]
+        )
+      end
+
+      attrs[:direct_message] = {
+        id: attrs[:id].to_i,
+        created_at: Time.at(attrs[:created_timestamp].to_i / 1000.0),
+        sender: { id: attrs[:message_create][:sender_id].to_i },
+        recipient: { id: attrs[:message_create][:target][:recipient_id].to_i },
+        text: text
+      }
+      super
+    end
+
   end
 end
 
