7eb0465450a919e321ab0a5dd9074315d9ce0ff9
WildDima <dtopornin@gmail.com>
2017-11-13 20:27:18 +0300

    Add specs to this particular case

--- spec/twitter/rest/request_spec.rb
+++ spec/twitter/rest/request_spec.rb
@@ -31,7 +31,7 @@ describe Twitter::REST::Request do
       it 'requests via the proxy when uploaded media is present' do
         stub_request(:post, 'https://upload.twitter.com/1.1/media/upload.json').to_return(body: fixture('upload.json'), headers: {content_type: 'application/json; charset=utf-8'})
         stub_post('/1.1/statuses/update.json').with(body: {status: 'Update', media_ids: '470030289822314497'}).to_return(body: fixture('status.json'), headers: {content_type: 'application/json; charset=utf-8'})
-        expect(HTTP).to receive(:via).with('127.0.0.1', 3328).twice.times.and_call_original
+        expect(HTTP).to receive(:via).with('127.0.0.1', 3328).twice.and_call_original
         @client.update_with_media('Update', fixture('pbjt.gif'))
       end
 
