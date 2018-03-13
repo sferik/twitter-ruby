71ef52aecfecc77fdcb0c33dc187e8aab3ab3e64
Kenji Okimoto <okimoto@clear-code.com>
2018-03-13 12:02:56 +0900

    Use port number 8443 instead of 11443
    Because rubocop reports following warnings:
    
        spec/twitter/streaming/connection_spec.rb:88:48: C: Use underscores(_) as decimal mark and separate every 3 digits with them.
            let(:client) {  TCPSocket.new('127.0.0.1', 11443) }
                                                       ^^^^^
        spec/twitter/streaming/connection_spec.rb:94:44: C: Use underscores(_) as decimal mark and separate every 3 digits with them.
              @server = TCPServer.new('127.0.0.1', 11443)
                                                   ^^^^^

--- spec/twitter/streaming/connection_spec.rb
+++ spec/twitter/streaming/connection_spec.rb
@@ -85,13 +85,13 @@ describe Twitter::Streaming::Connection do
 
     let(:method) { :get }
     let(:uri)    { 'https://stream.twitter.com:443/1.1/statuses/sample.json' }
-    let(:client) {  TCPSocket.new('127.0.0.1', 11443) }
+    let(:client) {  TCPSocket.new('127.0.0.1', 8443) }
 
     let(:request) { HTTP::Request.new(verb: method, uri: uri) }
     let(:response) { DummyResponse.new {} }
 
     before do
-      @server = TCPServer.new('127.0.0.1', 11443)
+      @server = TCPServer.new('127.0.0.1', 8443)
     end
 
     after do
