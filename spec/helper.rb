211924f5156c25e103483b7ad3cecca7f9717339
Erik Berlin <sferik@gmail.com>
2020-02-11 14:44:48 -0800

    Add official support for Ruby 2.6 and 2.7

--- spec/helper.rb
+++ spec/helper.rb
@@ -6,7 +6,7 @@ SimpleCov.formatters = [SimpleCov::Formatter::HTMLFormatter, Coveralls::SimpleCo
 SimpleCov.start do
   add_filter '/spec/'
   add_filter '/vendor/'
-  minimum_coverage(99.28)
+  minimum_coverage(99.85)
 end
 
 require 'twitter'
