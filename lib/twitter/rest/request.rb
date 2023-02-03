503ec278946b3e61a8b8c75a7c469e5a211adc75
Erik Berlin <sferik@gmail.com>
2023-02-03 13:54:38 -0800

    Convert if-elsif to case-when

--- lib/twitter/rest/request.rb
+++ lib/twitter/rest/request.rb
@@ -122,11 +122,12 @@ module Twitter
       end
 
       def symbolize_keys!(object)
-        if object.is_a?(Array)
+        case object
+        when Array
           object.each_with_index do |val, index|
             object[index] = symbolize_keys!(val)
           end
-        elsif object.is_a?(Hash)
+        when Hash
           object.dup.each_key do |key|
             object[key.to_sym] = symbolize_keys!(object.delete(key))
           end
