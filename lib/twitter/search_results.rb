73ed8175d3a33203f0f6f6dcba2eb25de468d0c2
Erik Berlin <sferik@gmail.com>
2023-02-03 15:35:13 -0800

    Prefer ary.to_h to Hash[ary]

--- lib/twitter/search_results.rb
+++ lib/twitter/search_results.rb
@@ -71,7 +71,7 @@ module Twitter
     #   query_string_to_hash("foo=bar&baz=qux") #=> {:foo=>"bar", :baz=>"qux"}
     def query_string_to_hash(query_string)
       query = CGI.parse(URI.parse(query_string).query)
-      Hash[query.collect { |key, value| [key.to_sym, value.first] }]
+      query.collect { |key, value| [key.to_sym, value.first] }.to_h
     end
   end
 end
