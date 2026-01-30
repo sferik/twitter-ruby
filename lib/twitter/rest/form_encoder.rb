498da84d0dd255640dcd71c39223807b7f726ebe
Erik Berlin <sferik@gmail.com>
2026-01-30 09:57:38 -0800

    Refactor Twitter::REST::FormEncoder

--- lib/twitter/rest/form_encoder.rb
+++ lib/twitter/rest/form_encoder.rb
@@ -4,24 +4,32 @@ module Twitter
       UNESCAPED_CHARS = /[^a-z0-9\-._~]/i
 
       def self.encode(data)
-        data.collect do |k, v|
-          if v.nil?
-            ::URI::DEFAULT_PARSER.escape(k.to_s, UNESCAPED_CHARS)
-          elsif v.respond_to?(:to_ary)
-            v.to_ary.collect do |w|
-              str = ::URI::DEFAULT_PARSER.escape(k.to_s, UNESCAPED_CHARS)
-              unless w.nil?
-                str << "="
-                str << ::URI::DEFAULT_PARSER.escape(w.to_s, UNESCAPED_CHARS)
-              end
-            end.join("&")
-          else
-            str = ::URI::DEFAULT_PARSER.escape(k.to_s, UNESCAPED_CHARS)
-            str << "="
-            str << ::URI::DEFAULT_PARSER.escape(v.to_s, UNESCAPED_CHARS)
-          end
-        end.join("&")
+        data.collect { |k, v| encode_pair(k, v) }.join("&")
       end
+
+      def self.encode_pair(key, value)
+        if value.nil?
+          escape(key)
+        elsif value.respond_to?(:to_ary)
+          encode_array(key, value.to_ary)
+        else
+          "#{escape(key)}=#{escape(value)}"
+        end
+      end
+
+      def self.encode_array(key, array)
+        array.collect { |item| encode_array_item(key, item) }.join("&")
+      end
+
+      def self.encode_array_item(key, item)
+        item.nil? ? escape(key) : "#{escape(key)}=#{escape(item)}"
+      end
+
+      def self.escape(value)
+        ::URI::DEFAULT_PARSER.escape(value.to_s, UNESCAPED_CHARS)
+      end
+
+      private_class_method :encode_pair, :encode_array, :encode_array_item, :escape
     end
   end
 end
